1652d91618ef3b7a0801d990fedd63bc27bbb52e
pionnertch <elaion256@outlook.com>
2014-12-21 23:26:21 -0200

    usuario back

--- backend/stsk-del.php
+++ backend/stsk-del.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $muser = $_GET['muser'];
 $user = $_GET['user'];
 $fechaF = $_GET['fechaF'];
@@ -8,14 +7,13 @@ $iss_id = $_GET['iss_id'];
 $stsk_id = $_GET['stsk'];
 $subject = $_GET['subject'];
 $descript = $_GET['descript'];
-$startD = $_GET['startD'];
+$startD = $_GET['startD'] . " 10:00:00";
 $fac = $_GET['fac'];
-
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-$query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE) ";
-$query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', '" . $user . "', '" . $fechaF . "', '" . $startD . "' , '" . $muser . "', " . $fac . ")";
+$query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_STATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE) ";
+$query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', '" . $user . "', '" . $fechaF . "', 1 ,  '" . $startD . "' , '" . $muser . "', " . $fac . ")";
 
 //
 
