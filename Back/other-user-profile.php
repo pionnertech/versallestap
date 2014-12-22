e450e1768ea5790afb6cffa55957cca8c4bee5f7
pionnertch <elaion256@outlook.com>
2014-12-22 14:48:23 -0200

    inser back again

--- Back/other-user-profile.php
+++ Back/other-user-profile.php
@@ -20,7 +20,7 @@ $Query_task = mysqli_query($datos, "SELECT A.STSK_ID, A.STSK_ISS_ID, A.STSK_SUBJ
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edmin</title>
+    <title>Eque-E</title>
     <link type="text/css" href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link type="text/css" href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link type="text/css" href="../css/theme.css" rel="stylesheet">  
