b723399a46837a411edb3c0df4a042f35f3c5798
pionnertch <elaion256@outlook.com>
2014-12-24 23:03:59 -0200

    table

--- backend/upgrade.php
+++ backend/upgrade.php
@@ -9,8 +9,6 @@ $descript = $_GET['des'];
 $date = $_GET['date'];
 $fac = $_GET['fac'];
 
-
-
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 mysqli_query($datos, "UPDATE SUBTASKS SET STSK_PROGRESS =  " . $val . " WHERE STSK_ID = " . $id . " ;");
