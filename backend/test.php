6f589ba2855aae5f1d308ee383d3bf18464f557c
pionnertch <elaion256@outlook.com>
2014-12-27 12:33:00 -0200

    lock & unlock

---
+++ backend/test.php
@@ -0,0 +1,15 @@
+
+<?php
+$directorio = opendir("."); //ruta actual
+while ($archivo = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
+{
+    if (is_dir($archivo))//verificamos si es o no un directorio
+    {
+        echo "[".$archivo . "]<br />"; //de ser un directorio lo envolvemos entre corchetes
+    }
+    else
+    {
+        echo $archivo . "<br />";
+    }
+}
+?>
