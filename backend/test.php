74ae3d4dbc3f3b7af790769fb2bb04a63c85ea2f
pionnertch <elaion256@outlook.com>
2014-12-27 12:41:19 -0200

    lock & unlock

--- backend/test.php
+++ backend/test.php
@@ -1,11 +1,15 @@
 
 <?php
-$directorio = opendir("."); //ruta actual
+$directorio = opendir("../10000/."); //ruta actual
 while ($archivo = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
 {
     if (is_dir($archivo))//verificamos si es o no un directorio
+
     {
-        echo "[".$archivo . "]<br />"; //de ser un directorio lo envolvemos entre corchetes
+       while($subfolder = readdir($archvo)){
+       	      echo  $subfolder;
+
+        } //de ser un directorio lo envolvemos entre corchetes
     }
     else
     {
