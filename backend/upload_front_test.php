7532b1b1454b9d6f95c5489e53eab1ec3a48cc54
pionnertch <elaion256@outlook.com>
2015-01-01 12:29:26 -0200

    rtest

--- backend/upload_front_test.php
+++ backend/upload_front_test.php
@@ -1,8 +1,8 @@
 <?php
 
+$fac = $_GET['fac'];
 
-echo $_GET['fac'];
-
+echo $fac;
 
 $dir = "/var/www/html/10000/";
 
@@ -11,8 +11,7 @@ if(!is_dir($dir . "temporary/")){
 	mkdir($dir . "temporary/", 0775, true);
 }
 
-
-if(move_uploaded_file($_FILES['file']['tmp-name']), $dir . "temporary/" . $_FILES['file']['name'])){
+if(move_uploaded_file($_FILES['file']['tmp_name'], $dir . "temporary/" . $_FILES['file']['name'])) {
 	echo "was uploaded";
 }
 
