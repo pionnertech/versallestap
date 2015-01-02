6b8981ec3161e93717518b327aa5dadf60d0e00b
pionnertch <elaion256@outlook.com>
2015-01-02 14:04:26 -0200

    move uoloads

--- backend/move.php
+++ backend/move.php
@@ -14,16 +14,18 @@ $rut = mysqli_fetch_assoc(mysqli_query($datos, "SELECT ISS_CTZ FROM ISSUES WHERE
 $basedir = "/var/www/html/" . $fac . "/";
 $targetdir = $basedir . "temporary/";
 
-if(!is_dir($basedir . "usr_id/")){
-	mkdir($basedir . "usr_id/", 0775, true);
+if(!is_dir($basedir . $usr_id . "/")){
+	mkdir($basedir . $usr_id . "/", 0775, true);
 }
 
 //retive all file matching rut 
  if ($file_array = opendir("../" . $_SESSION['TxtFacility'] . "/temporary/" )){
        while (false !== ($archivos = readdir($file_array))){
            if(preg_match_all("/_" . $rut['ISS_CTZ'] . "_/", $archivos) == 1){
-                 if(move_uploaded_file($archivos, $basedir . "/" . $sr_id . "/" . $archivos )){
+                 if(move_uploaded_file($archivos, $basedir . "/" . $usr_id . "/" . $archivos )){
                  	echo "was upload_" . $archivos;
+                 } else {
+                 	echo "no se pudo";
                  }
            }
 
