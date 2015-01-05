6f6c5d0f623541dbd501b822bcc6a8ac6eea14e7
pionnertch <elaion256@outlook.com>
2015-01-05 00:47:59 -0200

    drag and drop

---
+++ backend/switchfiles.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$fac = $_GET['fac'];
+$file_name = $_GET['file_name'];
+$main_usr_id = $_GET['main_usr_id'];
+$charge_usr_id = $_GET['charge_usr_id'];
+
+//first check if exist
+$dir = "/var/www/html/" . $fac . "/";
+if(!is_file($dir . $main_usr_id . "/" . $file_name)){
+	echo "no file!";
+	exit;
+}
+
+ if(copy($dir . $main_usr_id . "/" . $file_name, $dir . $main_usr_id . "/" . $file_name )){
+ 	echo "was copied";
+ 	unlink($dir . $main_usr_id . "/" . $file_name);
+ }
+?>
