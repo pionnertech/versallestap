02906b0ac3b02d32cd2da6a1547dad4ac207fad7
pionnertch <elaion256@outlook.com>
2015-01-05 00:57:36 -0200

    drag and drop

--- Admin/other-user-profile-test.php
+++ Admin/other-user-profile-test.php
@@ -1106,8 +1106,8 @@ border-radius: 20px;
 <script type="text/javascript">
     
     var st = 0;
-    var fac = <? $_SESSION['TxtFacility'] ?>
-    var mainuser = <? $_SESSION['TxtCode'] ?>
+    var fac = <? printf($_SESSION['TxtFacility'] ) ?>
+    var mainuser = <? printf($_SESSION['TxtCode'])  ?>
     $(document).on('ready', function(){
 
 
