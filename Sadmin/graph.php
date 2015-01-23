b5d15b7ce8392d1e749fa1e73904617817c89121
pionnertch <elaion256@outlook.com>
2015-01-23 17:04:08 -0200

    black heart

--- Sadmin/graph.php
+++ Sadmin/graph.php
@@ -111,7 +111,7 @@ $i = 0;
 $query_count_departament = mysqli_query($datos, "SELECT DISTINCT B.USR_DEPT FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID)  WHERE STSK_FAC_CODE = " . $_SESSION['TxtFacility'] . " GROUP BY USR_DEPT;");
 while($f1 = mysqli_fetch_row($query_count_departament)){
 ?>
-                                   <div class="chart inline-legend grid linerchart" align="center">
+                                   <div class="chart inline-legend grid linerchart" align="center" style="width: 100%;">
                                         <div id="chart<? printf($i)?>" class="graph" style="height: 200px"></div>
                                         <strong><? printf($f1[0]) ?></strong>
                                     </div>
