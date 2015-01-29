89b8af798840a58d27cc695aed40139c1415328e
pionnertch <elaion256@outlook.com>
2015-01-29 05:16:05 -0200

    JSON

--- backend/JSON.php
+++ backend/JSON.php
@@ -101,7 +101,7 @@ for($i=0; $i < $cant_dept; $i++){
                         
                         $w = $w + 1;
 
-                        if($w == $per_count ){
+                        if($w <= mysqli_num_rows($hand_per)){
                                
                          } else {
                                
