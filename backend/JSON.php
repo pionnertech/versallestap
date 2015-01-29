f68ad953a958569de93c7978f245fdaa82235799
pionnertch <elaion256@outlook.com>
2015-01-29 02:42:48 -0200

    JSON

--- backend/JSON.php
+++ backend/JSON.php
@@ -91,17 +91,25 @@ for($i=0; $i < $cant_dept; $i++){
             
                         echo "{\"label\":\"" . $fila2[2]  . "\", \"data\": \"" . $fila2[0] . "\", \"color\":\"" . $fila2[1] . "\"}";
                         
-                            if($y == $per_count ){
+                        if($y == $per_count ){
                                
-                            } else {
+                         } else {
                                
                                echo ",";
                                
-                            }
+                            }   
+
                     }
 
                 echo "]"; 
         
+                     if($y == $per_count ){
+                               
+                     } else {
+                               
+                         echo ",";
+                               
+                       }
             }
 
 //==========================
