454b84329c4125b35b41e9ce465b07ce273b9cb8
pionnertch <elaion256@outlook.com>
2015-02-02 12:21:48 -0200

    chevron

--- Back/other-user-profile.php
+++ Back/other-user-profile.php
@@ -458,7 +458,7 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ff5335', end
                                             <td class="cell-icon"><i class="icon-checker high"></i></td>
                                             <td class="cell-title"><div><? printf($stsk[3]) ?></div></td>
                                             <td class="cell-status hidden-phone hidden-tablet"><b class="due" style="background-color: <? printf($stsk[6]) ?> !important;"><? printf($stsk[5]) ?></b></td>
-                                            <td class="cell-title"><button class="btn btn-inverse forward">Marcar avance</button></td>
+                                            <td class="cell-title"><button class="btn btn-default forward"><i class="fa fa-chevron-circle-right"></i></button></td>
                                             <td class="cell-time"><div><? printf($stsk[4]) ?></div></td>
                                             <td class="cell-time align-right"><div><? printf(date("d/m/Y", strtotime(substr($stsk[7], 0, 10)))) ?></div></td>
                                             <input type="hidden" value="<? printf($stsk[0]) ?>" >
