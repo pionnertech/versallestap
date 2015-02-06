a64875cffd890105bb35eda5da388574e0c0fe1a
pionnertch <elaion256@outlook.com>
2015-02-06 19:36:22 -0200

    JSON bourne

---
+++ backend/upload_back_to_admin.php
@@ -0,0 +1,44 @@
+<?php
+
+$fac  = $_REQUEST['fac'];
+$code = $_REQUEST['code'];
+$user = $_REQUEST['user'];
+
+
+
+$target_dir = "/var/www/html/" . $fac . "/";
+$target_file = $target_dir . basename($_FILES["upl"]["name"]);
+$uploadOk = 1;
+
+
+if(!is_dir($target_dir)){
+	mkdir($target_dir, 0775, true);
+}
+
+
+if(!is_dir($target_dir . $user . "_in/")){
+	chmod($target_dir . $user . "_in/");
+	mkdir($target_dir . $user . "_in/", 0775, true);
+}
+
+// A list of permitted file extensions
+$allowed = array('png', 'jpg', 'gif','zip', 'docx', 'xls', 'xlsx', 'pdf', 'doc','ppt' );
+
+if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
+
+	$extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
+
+	if(!in_array(strtolower($extension), $allowed)){
+		echo '{"status":"error"}';
+		exit;
+	}
+	if(move_uploaded_file($_FILES['upl']['tmp_name'] , $target_dir . "/" . $user . "_in/" . basename($_FILES['upl']['name'] , "." . strtolower($extension)) . "_" . $code . "_" . $user . "." . strtolower($extension) )){
+		echo '{"status":"success"}';
+		exit;
+	}
+}
+
+echo '{"status":"error"}';
+exit;
+
+?>
