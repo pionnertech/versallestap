1a1d71a058ce95dc277051b9be52c8fd0167c637
pionnertch <elaion256@outlook.com>
2015-02-09 18:20:15 -0200

    refresh delegates

--- backend/stsk-del.php
+++ backend/stsk-del.php
@@ -21,7 +21,7 @@ echo 0;
 
 } else {
 
- $handler = mysqli_query($datos, "SELECT STSK_CHARGE_USR WHERE STSK_ISS_ID = " . $iss_id);
+ $handler = mysqli_query($datos, "SELECT STSK_CHARGE_USR FROM SUBTASKS WHERE STSK_ISS_ID = " . $iss_id);
  while ($fila1v = mysqli_fetch_row($handler)){
  	echo $fila1v .",";
  }
