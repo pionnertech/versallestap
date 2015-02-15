cad8b0ca5cdb249c1d4c1cef3b5df436ddf09970
pionnertch <elaion256@outlook.com>
2015-02-15 19:06:30 -0200

    JSON injection

--- Front/multi.php
+++ Front/multi.php
@@ -13,7 +13,7 @@ $query_count_departament = mysqli_query($datos, "SELECT DISTINCT B.USR_DEPT FROM
 
 $depts = mysqli_query($datos, "SELECT DISTINCT B.USR_DEPT FROM SUBTASKS A INNER JOIN USERS B  ON(A.STSK_CHARGE_USR = B.USR_ID) WHERE STSK_FAC_CODE = 10000 GROUP BY USR_DEPT");
 
-$parray = [];
+$parray = array();
 
 $i = 0;
 
