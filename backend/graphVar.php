551361d2eae4d918dea82403c2cec45be714b5ad
pionnertch <elaion256@outlook.com>
2015-02-20 04:46:25 -0200

    pseudo JSON

--- backend/graphVar.php
+++ backend/graphVar.php
@@ -9,7 +9,7 @@ $graph_query = " SELECT B.EST_COLOR, B.EST_DESCRIPT, COUNT( A.STSK_ID ) , " .
                " FROM SUBTASKS A RIGHT JOIN EST B ON " .
                " ( A.STSK_STATE = B.EST_CODE AND A.STSK_CHARGE_USR = " . $user_id . ") " .
                " CROSS JOIN ( SELECT COUNT(STSK_ID )" .
-               "  AS total FROM SUBTASKS WHERE STSK_CHARGE_USR = " . $user_id . " ) x GROUP BY 1 ORDER BY EST_DESCRIPT";
+               "  AS total FROM SUBTASKS WHERE STSK_CHARGE_USR = " . $user_id . " ) x GROUP BY 1 ORDER BY EST_CODE";
 
 $graph = mysqli_query($datos, $graph_query);
 
