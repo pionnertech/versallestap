ce3f97e980f3f951d112f2741e4f010e22c18ddb
pionnertch <elaion256@outlook.com>
2015-02-22 19:04:58 -0300

    enhance spechar

--- backend/specialcharacter.php
+++ backend/specialcharacter.php
@@ -36,7 +36,7 @@ while($extra = mysqli_fetch_row($depts)){
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Eque-e</title>
+        <title>Cumplimos.cl</title>
         <link type="text/css" href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link type="text/css" href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link type="text/css" href="../css/theme.css" rel="stylesheet">
@@ -66,31 +66,12 @@ while($extra = mysqli_fetch_row($depts)){
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">Eque-e</a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html"><img src="images/valpo.jpg" style="max-width:3em;"></a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
                         <ul class="nav nav-icons">
                             <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
                             <li><a href="#"><i class="icon-eye-open"></i></a></li>
                             <li><a href="#"><i class="icon-bar-chart"></i></a></li>
-                        </ul>
-                        <form class="navbar-search pull-left input-append" action="#">
-                        <input type="text" class="span3">
-                        <button class="btn" type="button">
-                            <i class="icon-search"></i>
-                        </button>
-                        </form>
-                        <ul class="nav pull-right">
-                            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <b class="caret"></b></a>
-                            </li>
-                           
-                            <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="images/user.png" class="nav-avatar" />
-                                <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Logout</a></li>
-                                </ul>
-                            </li>
                         </ul>
                     </div>
                     <!-- /.nav-collapse -->
@@ -103,14 +84,6 @@ while($extra = mysqli_fetch_row($depts)){
             <div class="container">
                 <div class="row">
                     <div class="span3">
-                        <div class="sidebar">
-                            <ul class="widget widget-menu unstyled">
-                                <li><a href="message.html"><i class="menu-icon icon-inbox"></i>Página principal<b class="label green pull-right">
-                                    11</b> </a></li>
-                                <li><a href="task.html"><i class="menu-icon icon-tasks"></i><b class="label orange pull-right">
-                                    19</b> </a></li>
-                            </ul>
-                        </div>
                         <!--/.sidebar-->
                     </div>
                     <!--/.span3-->
