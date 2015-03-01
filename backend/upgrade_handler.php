61fcee998aca2473631f7ca282f0129c48a78c72
pionnertch <elaion256@outlook.com>
2015-03-01 15:31:42 -0300

    latest changes

--- backend/upgrade_handler.php
+++ backend/upgrade_handler.php
@@ -18,8 +18,11 @@ $str_traffic = "SELECT A.TRF_STSK_SRC_ID,  " .
 "A.TRF_USER, CONCAT(B.USR_NAME , ' ' ,  B.USR_SURNAME)  FROM TRAFFIC A INNER JOIN USERS B ON(A.TRF_USER = B.USR_ID) " . 
 "WHERE (TRF_FAC_CODE = " . $fac . " AND TRF_STSK_SRC_ID = " . $id_s . ");";
 
+echo $str_traffic;
+
 $handler = mysqli_query($datos, $str_traffic);
 $num = mysqli_num_rows($handler);
+
 $i= 0;
 echo "{\"datos\":[";
 
