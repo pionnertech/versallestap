449cfbcce75c2ac39bf6907c4b8d51fb6e7c7286
pionnertch <elaion256@outlook.com>
2015-03-02 16:45:22 -0300

    4  & 5

--- backend/graphVar.php
+++ backend/graphVar.php
@@ -43,7 +43,7 @@ $x = 0;
        mysqli_data_seek($graph, 0);
   
         while ( $fila = mysqli_fetch_row($graph)){
-          echo $fila[0] . " / " . round(($fila[0] / $x) * 100);
+          echo $fila[0] . " / " . round(($fila[0] / $x) * 10);
         }
    
 
