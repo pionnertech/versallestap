b4b1f99e89a3e4fc0dd88755d1080a9ca59d0ffa
pionnertch <elaion256@outlook.com>
2015-03-10 14:26:37 -0300

    dynamics JSON

---
+++ backend/dynamics_JSON_files.php
@@ -0,0 +1,21 @@
+<?php 
+
+$a = $_GET['usr_id']:
+$iss_id = $_GET_['iss_id'];
+
+
+if($hdir = opendir("/var/www/html/" . $fac . "/" . $a . "/")){
+
+     while (false !== ($files = readdir($hdir))){
+
+     	 if(preg_match_all("/_\[" . $iss_id  . "\]_/", $files) == 1){
+
+     	 	 $outcome .= $files . "|";
+     	 }
+     }
+}
+
+echo $outcome;
+
+?>
+
