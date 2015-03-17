f2272cdab13128280cb3c57fee918de1f53d6683
pionnertch <elaion256@outlook.com>
2015-03-17 17:57:48 -0300

    dynamic admin

--- backend/time-progress-event.php
+++ backend/time-progress-event.php
@@ -4,6 +4,7 @@
 $iss_stsk    = $_GET['st'];
 $percent     = $_GET['per'];
 $stsk_src_id = $_GET['srcid'];
+$usr         = $_GET['usr'];
 
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
@@ -12,27 +13,32 @@ $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 while (true) {
 	
-$handler    = mysqli_query($datos, "SELECT SUM(STSK_PROGRESS) FROM SUBTASKS WHERE (STSK_ISS_ID = " . $iss_stsk . " AND STSK_CHARGE_USR != STSK_MAIN_USR) GROUP BY STSK_CHARGE_USR ");
-$count      = mysqli_num_rows($handler);
 
-while ($fila = mysqli_fetch_row($handler)){
-	$sum += $fila[0];
-}
+$news = mysqli_query($datos, "SELECT STSK_PROGRESS , STSK_ANCIENT_PRO, STSK_ID, STSK_ISS_ID FROM SUBTASKS WHERE STSK_MAIN_USR = "  . $usr);
 
-$ctp   = (($sum * 100) / (100 * $count));
+while( $new = mysqli_fetch_row($news)){
 
-$classText = "";
+	if($new[0] != $new[1]){
 
-if ($ctp >= 99.9 ){
+       mysqli_query($datos, "UPDATE TABLE SUBTASKS SET STSK_ANCIENT_PRO = " . $new[0] . " WHERE STSK_ID =" . $new[2]);
 
-   $classText = "FINALIZADO";
+       $handler = mysqli_query($datos, "SELECT SUM(STSK_PROGRESS) FROM SUBTASKS WHERE (STSK_ISS_ID = " . $new[3] . " AND STSK_CHARGE_USR != STSK_MAIN_USR) GROUP BY STSK_CHARGE_USR ");
+       $count   = mysqli_num_rows($handler);
 
-}
+       while ($fila = mysqli_fetch_row($handler)){
+	         $sum += $fila[0];
+          }
+        $ctp   = (($sum * 100) / (100 * $count));
 
-//GEt the Last User that grow up his progress
+        $classText = "";
 
+        if ($ctp >= 99.9 ){
 
-$query_usr = mysqli_query($datos, "SELECT CONCAT(B.USR_NAME, ' ', B.USR_SURNAME) AS NAME, A.TRF_USER,  A.TRF_SUBJECT, A.TRF_DESCRIPT, A.TRF_ING_DATE, A.TRF_STSK_SRC_ID FROM TRAFFIC A INNER JOIN USERS B ON(A.TRF_USER = B.USR_ID) WHERE A.TRF_STSK_SRC_ID = " . $stsk_src_id . " ORDER BY TRF_ID DESC LIMIT 1" );
+           $classText = "FINALIZADO";
+
+          }
+
+$query_usr = mysqli_query($datos, "SELECT CONCAT(B.USR_NAME, ' ', B.USR_SURNAME) AS NAME, A.TRF_USER,  A.TRF_SUBJECT, A.TRF_DESCRIPT, A.TRF_ING_DATE, A.TRF_STSK_SRC_ID FROM TRAFFIC A INNER JOIN USERS B ON(A.TRF_USER = B.USR_ID) WHERE A.TRF_STSK_SRC_ID = " . $new[2] . " ORDER BY TRF_ID DESC LIMIT 1" );
 $user      = mysqli_fetch_assoc($query_usr);
 
 sleep(1);
@@ -41,10 +47,20 @@ echo "data :" . str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtol
 echo "data :" . $user['TRF_USER'] .  "\n";
 echo "data :" . $user['TRF_SUBJECT'] .  "\n";
 echo "data :" . $user['TRF_DESCRIPT'] .  "\n";
-echo "data :" . $user['TRF_ING_DATE'] . "\n";
-echo "data :" . $user['TRF_STSK_SRC_ID'] .  "\n";
+echo "data :" . date('d/m/Y', strtotime($user['TRF_ING_DATE'] )) .  "\n";
+echo "data :" . $user['TRF_STSK_SRC_ID'] . "\n";
 echo "data :" . $ctp .  "\n";
 echo "data :" . $classText .  "\n\n";
+
+
+	}  else {
+      sleep(1);
+     echo "data :" . "\n";
+     echo "data :" . "\n";
+}
+
+//GEt the Last User that grow up his progress
+ }
 
 ob_end_flush();
 flush();
