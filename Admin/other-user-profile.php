a4b1ea36ed506790109f10cc47579066fb5fd64b
pionnertch <elaion256@outlook.com>
2015-03-22 23:17:11 -0300

    join to another connection

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -1,23 +1,5 @@
 <?php session_start();
 
-  // initialize ob_gzhandler function to send and compress data
-   ob_start ("ob_gzhandler");
- 
-   // send the requisite header information and character set
-   header ("content-type: text/javascript; charset: UTF-8");
- 
-   // check cached credentials and reprocess accordingly
-   header ("cache-control: must-revalidate");
- 
-   // set variable for duration of cached content
-   $offset = 60 * 60;
- 
-   // set variable specifying format of expiration header
-   $expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";
- 
-   // send cache expiration header to the client broswer
-   header ($expire);
-
 if(isset($_SESSION['TxtCode']) && $_SESSION['TxtRange'] === 'admin'){
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
