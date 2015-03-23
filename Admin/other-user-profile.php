24ccb81929c13a579c4341c63e40e0581b982113
pionnertch <elaion256@outlook.com>
2015-03-23 16:06:59 -0300

    trying

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -1574,7 +1574,7 @@ var iconShow = "http://icons.iconarchive.com/icons/visualpharm/must-have/256/Nex
                        showAlert(packets[2], "pro", packets[0]);
                        collection = $(".input.st");
                        indice = $("input.st[value=" + packets[5]+ "]").index(collection);
-                       updateProgress(packets[2], packets[3], packets[6], packets[4], packets[1], packets[0], 3, packets[5]);
+                       updateProgress(packets[2], packets[3], packets[6], packets[4], packets[1], packets[0], indice, packets[5]);
                  }
             }
         })
