45427eaf623a82ca729a4de199bd4527251acb24
pionnertch <elaion256@outlook.com>
2015-03-24 08:23:58 -0300

    Enhanced

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -1063,10 +1063,10 @@ var subject = $(this).parent().parent().children('td').eq(1).text();
 var index_current = parseInt($(this).index());
 
 var proc = $(this).parent().next().children().html();
-
+console.info(proc +  "    /win");
 var dm = new Date(proc);
-
-dateTime   = dm.getFullYear() + "/"  + ("0" + dm.getDate()).slice(-2) + "/" + ("0" + (dm.getMonth() + 1 )).slice(-2);
+console.info(dm  + "   / win")
+dateTime   = dm.getFullYear() + "/"  + ("0" + dm.getDate()).slice(-2) + "/" + ("0" + (dm.getMonth() + 1)).slice(-2);
 
 $("#audititle").html("\"" + subject + "\"");
 $("#current-task").val(index_current);
