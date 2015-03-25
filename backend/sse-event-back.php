723e4be7be4658c68fc48812ad4b339e6d68165a
pionnertch <elaion256@outlook.com>
2015-03-25 11:48:24 -0300

    back intwernals

--- backend/sse-event-back.php
+++ backend/sse-event-back.php
@@ -14,7 +14,8 @@ $str_query = "SELECT A.STSK_ID, " .
 "B.ISS_DESCRIP, " .
 "CONCAT(C.CTZ_NAMES, ' ' , C.CTZ_SURNAME1, ' ', C.CTZ_SURNAME2) AS NAME , " .
 "A.STSK_FINISH_DATE, AS FECHA_FINAL,  " .
-"A.STSK_START_DATE, AS FECHA_INICIAL  " .
+"A.STSK_START_DATE, AS FECHA_INICIAL,  " .
+"A.STSK_TYPE " . 
 " FROM SUBTASKS A INNER JOIN ISSUES B ON(B.ISS_ID = A.STSK_ISS_ID) INNER JOIN CITIZENS C ON (B.ISS_CTZ = C.CTZ_RUT) " . 
 "WHERE ( STSK_CHARGE_USR = "  . $a . " AND STSK_LOCK = 1) ORDER BY STSK_ID DESC LIMIT 1 ";
 
@@ -31,7 +32,8 @@ echo "data:" . $manu['STSK_DESCRIP'] . "\n";
 echo "data:" . $manu['ISS_DESCRIP'] . "\n";
 echo "data:" . $manu['NAME'] . "\n";
 echo "data:" . date('d/m/Y', strtotime($manu['FECHA_FINAL'])) . "\n";
-echo "data:" . date('d/m/Y', strtotime($manu['FECHA_INICIAL'])) . "\n\n";
+echo "data:" . date('d/m/Y', strtotime($manu['FECHA_INICIAL'])) . "\n";
+echo "data:" . $manu['STSK_TYPE'] . "\n\n";
 
 ob_end_flush();
 flush();
