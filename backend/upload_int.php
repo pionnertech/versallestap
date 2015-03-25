02f6a02d936a62a8ca579442b2c38d9d10a14d31
pionnertch <elaion256@outlook.com>
2015-03-25 14:34:39 -0300

    back intwernals

--- backend/upload_int.php
+++ backend/upload_int.php
@@ -1,12 +1,12 @@
 <?php
 
 $fac    = $_REQUEST['fac_id'];
-$iss_id = $_REQUEST['stsk'];
 $user   = $_REQUEST['user'];
 $keyfile = $_REQUEST['keyfile'];
 
 if(!isset($keyfile)){
 	$dir = "/var/www/html/" . $fac . "/" . $user . "_alt/";
+	$iss_id = $_REQUEST['stsk'];
 } else {
 	$dir = "/var/www/html/" . $fac . "/_tmp/";
 	$iss_id = $keyfile;
@@ -18,7 +18,7 @@ if(!is_dir($dir)){
 
 $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 if(move_uploaded_file($_FILES['file']['tmp_name'] , $dir . basename($_FILES['file']['name'], "." . strtolower($extension)) . "_[" . $iss_id . "]_." . $extension  )) {
-	echo $dir . basename($_FILES['file']['name'], "." . strtolower($extension)) . "_[" . $iss_id . "]_." . $extensionn  ;
+	echo $dir . basename($_FILES['file']['name'], "." . strtolower($extension)) . "_[" . $iss_id . "]_." . $extension ;
 }
 
 ?>
