1b6f1be37ffdfbbe5c2e68e2e1d20ef60c4fe842
pionnertch <elaion256@outlook.com>
2015-03-29 12:50:04 -0300

    new of real time

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -1665,6 +1665,7 @@ var iconShow = "http://icons.iconarchive.com/icons/visualpharm/must-have/256/Nex
                        updateProgress(packets[2], packets[3], packets[6], packets[4], packets[1], packets[0], indice, packets[5] );
                        if(parseInt(packets[8]) >= 99.5){
                            $(".collaborates").eq(indice).children("input[value=u" + packets[4] +"]").prev().css({ opacity : "1"});
+                           
                        }
 
                  }
