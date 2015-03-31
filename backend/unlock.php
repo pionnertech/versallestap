93236772e2f0a52e4560b0ea00a51b505fc921b3
pionnertch <elaion256@outlook.com>
2015-03-31 04:31:04 -0300

    backend

--- backend/unlock.php
+++ backend/unlock.php
@@ -6,14 +6,14 @@ $fac = $_GET['fac'];
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-if(!mysqli_query($datos,"UPDATE SUBTASKS SET STSK_LOCK = 1 WHERE STSK_ID= " . $stsk_id )){
+if(!mysqli_query($datos,"UPDATE SUBTASKS SET STSK_LOCK = 1 WHERE (STSK_ID = " . $stsk_id . " AND STSK_FAC_CODE = " . $fac . ")" )){
 	
 	echo 0;
 
 } else {
 
-if($iss_id){
-     mysqli_query($datos,"UPDATE SUBTASKS SET STSK_LOCK = 1 WHERE STSK_ISS_ID = " . $iss_id);
+if(isset($iss_id)){
+     mysqli_query($datos,"UPDATE SUBTASKS SET STSK_LOCK = 1 WHERE (STSK_ISS_ID = " . $iss_id . " AND STSK_FAC_CODE = " . $fac . ")");
 }
 	echo 1;
 
