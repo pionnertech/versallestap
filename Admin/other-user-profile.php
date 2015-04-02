bc01c2cd39bb9c5ddb47726f814d43f995e0ebfc
pionnertch <elaion256@outlook.com>
2015-04-01 23:42:41 -0300

    positivo

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -910,7 +910,7 @@ $Query_traffic =  mysqli_query($datos, $str_traffic);
 
                                            while (false !== ($archivos2 = readdir($handler2))){
                                       echo "<script>console.info('" . $archivos2 . "' + '/' + '" . $fint[0] . "' + '/' + '" . $fila5[0] . "' )</script>";
-                                      echo "<script>console.info('" +  preg_match_all("/_\[" . $fila5[0] . "\]_/", $archivos2) + "')</script>";
+                                      echo "<script>console.info('" .  preg_match_all("/_\[" . $fila5[0] . "\]_/", $archivos2) . "')</script>";
                                          if(preg_match_all("/_\[" . $fila5[0] . "\]_/", $archivos2) == 1){
 
                                              echo "<script>console.info('" +  $archivos2 + "')</script>";
