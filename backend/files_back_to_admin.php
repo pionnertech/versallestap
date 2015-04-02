03cbac9e92de5c8dc4e3dcf49df66e51288806cf
pionnertch <elaion256@outlook.com>
2015-04-02 17:43:00 -0300

    positivo

--- backend/files_back_to_admin.php
+++ backend/files_back_to_admin.php
@@ -3,8 +3,11 @@
 $fac  = $_REQUEST['fac'];
 $user = $_REQUEST['user'];
 $stsk = $_REQUEST['stsk'];
+$kind = $_REQUEST['kind'];
 
-if($hdir = opendir("/var/www/html/" . $fac . "/" . $user . "_in/")){
+if($kind == 0 || $kind == "0"){
+   $rdir = "/var/www/html/" . $fac . "/" . $user . "_in/";
+   if($hdir = opendir($rdir)){
 
      while (false !== ($files = readdir($hdir))){
 
@@ -13,6 +16,21 @@ if($hdir = opendir("/var/www/html/" . $fac . "/" . $user . "_in/")){
      	 }
      }
 }
+} else {
+	
+   $rdir = "/var/www/html/" . $fac . "/" . $user . "_alt/";
+   if($hdir = opendir($rdir)){
+
+     while (false !== ($files = readdir($hdir))){
+
+     	 if(preg_match_all("/_\[" . $stsk  . "\]_/", $files) == 1){
+     	 	 $outcome .= $files . "|";
+     	 }
+     }
+}
+}
+
+
 echo $outcome;
 
 
