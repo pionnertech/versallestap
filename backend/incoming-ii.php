9aba30e727683af903177fbb5f09cb60bb329332
pionnertch <elaion256@outlook.com>
2015-04-03 09:25:06 -0300

    positivo

--- backend/incoming-ii.php
+++ backend/incoming-ii.php
@@ -10,18 +10,21 @@ $str_query = "SELECT STSK_ID, " .
 "STSK_DESCRIP, " .
 "STSK_FINISH_DATE AS FECHA_FINAL,  " .
 "STSK_START_DATE AS FECHA_INICIAL,  " .
-"STSK_TYPE " . 
+"STSK_TYPE, " . 
+"STSK_PROGRESS  " . 
 " FROM SUBTASKS  " . 
 "WHERE ( STSK_CHARGE_USR = "  . $a . " AND STSK_LOCK = 1 AND STSK_TYPE = 1 AND STSK_MAIN_USR <> STSK_CHARGE_USR AND STSK_ANCIENT <> STSK_PROGRESS) ORDER BY STSK_ID DESC";	
 
 $hand = mysqli_query($datos, $str_query);
 
+if(mysqli_num_rows($hand) != 0){
 while ($manu = mysqli_fetch_row($hand)){
 
 echo $manu[0] . "|" . $manu[1] . "|" . $manu[3] . "|" . date('d/m/Y', strtotime($manu[4])) . "|" . date('d/m/Y', strtotime($manu[5])) . "|" . $manu[6] . "|\n";
 
+   }
+   $hand2 = mysqli_query($datos, "UPDATE SUBTASKS SET STSK_ANCIENT = STSK_PROGRESS WHERE  (STSK_CHARGE_USR = "  . $a . " AND STSK_LOCK = 1 AND STSK_TYPE = 1 AND STSK_MAIN_USR <> STSK_CHARGE_USR AND STSK_ANCIENT <> STSK_PROGRESS )");
 }
-
 
 
 
