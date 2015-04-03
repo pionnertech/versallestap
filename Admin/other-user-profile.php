dedce7d6a7f76ab65270051e8a1ac1dc226a07be
pionnertch <elaion256@outlook.com>
2015-04-03 01:54:13 -0300

    positivo

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -2109,7 +2109,7 @@ var iconShow = "http://icons.iconarchive.com/icons/visualpharm/must-have/256/Nex
                             updateProgress(packets[2], packets[3], packets[6], packets[4], packets[1], packets[0], indice, packets[5], packets[9]);
 
                            if(parseInt(packets[8]) >= 99.5){
-                                $(".collaborates").eq(indice).children(".hovetip").children("input[value=u" + packets[4] +"]").prev().css({ opacity : "1"});
+                                $(".collaborates").eq(indice).children(".hovetip").children("input[value=u" + packets[5] +"]").prev().css({ opacity : "1"});
                                 $(".finished").eq(indice).css({opacity : "1"});
                         }
                     }
@@ -2469,11 +2469,10 @@ tr_av.appendChild(td1_av);
 tr_av.appendChild(td2_av);
 tr_av.appendChild(td3_av);
 
-
 $.ajax({ type:"POST",
          url: "../backend/files_back_to_admin.php?fac=" + fac +  "&user=" + userId + "&stsk=" + stsk + "&kind=" + kind,
          success : function (data){
-        console.info(data);
+            console.info(data);
             files = data.split("|");
 
         for (n=0; n < files.length-1 ; n++){
