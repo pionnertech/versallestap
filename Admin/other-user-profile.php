e828033a27d95a2cf4e12b395afb449f01e9ada8
pionnertch <elaion256@outlook.com>
2015-04-05 01:04:21 -0300

    JSON

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -1764,7 +1764,7 @@ $manu['STSK_TYPE'];
                             if(alpha[1] !== undefined){
                             firstTask(alpha[0], alpha[2], "Administrador" , alpha[3], alpha[6], 0);
                                 console.info( alpha[0] + "/" + alpha[1] + "/" + alpha[2] + "/" + alpha[3] +  "/" + alpha[4]);
-                                   showAlert(alpha[7], "ii" , alpha[1]);
+                                   showAlert(alpha[2], "ii" ,  alpha[7]);
                                    thum(1, "ii", "En Curso");
                             }
                        }
