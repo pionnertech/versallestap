01f99135b5d1ea857f584b9457ff629657961d5d
pionnertch <elaion256@outlook.com>
2015-04-06 10:17:48 -0300

    jiujkiuj

--- Back/other-user-profile.php
+++ Back/other-user-profile.php
@@ -499,9 +499,9 @@ $trf_hand = mysqli_query($datos, $str_query_trf);
 
                                            while (false !== ($archivos2 = readdir($handler))){
 
- echo "<script>console.info('"  . $archivos2 . "' + '/' + '" . $fila_int[1]  . "/' + '" . preg_match_all("/_\[" . $fila_int[1] . "\]_/", $archivos2) . "')</script>";
+ echo "<script>console.info('"  . $archivos2 . "' + '/' + '" . $fila_int[0]  . "/' + '" . preg_match_all("/_\[" . $fila_int[0] . "\]_/", $archivos2) . "')</script>";
 
-                                         if(preg_match_all("/_\[" . $fila_int[1] . "\]_/", $archivos2) == 1){
+                                         if(preg_match_all("/_\[" . $fila_int[0] . "\]_/", $archivos2) == 1){
                                              
                                              $extension = substr($archivos2, -3);
                                               $cor = "";
