f0f4b60a0e762d408a2f0313796f09f2c5ebd061
pionnertch <elaion256@outlook.com>
2015-04-10 13:41:48 -0300

    tudo bem

--- Back/other-user-profile.php
+++ Back/other-user-profile.php
@@ -883,9 +883,10 @@ console.info("../backend/upgrade.php?val=" + val +
                 }
 
              } else {
-              console.info("index + 1 : " + (index +1))
+              var weirdIndex =  (index-1)/2;
+              
             $("#int-table > tbody > tr").eq(index + 1).children("td").children('p').children('span').html(val + "%");
-            $("#int-table .bar").eq((index - 1)/2).children("td").children('div').eq(0).children(".bar").css({ width: val + "%"});
+            $("#int-table .bar").eq(weirdIndex).children("td").children('div').eq(0).children(".bar").css({ width: val + "%"});
 
             holindex = index;
             index = (index-1)/2;
