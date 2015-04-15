f04d393e34951158c615fd299f5804aaea9d20af
pionnertch <elaion256@outlook.com>
2015-04-14 23:44:41 -0300

    latest

--- backend/emer.php
+++ backend/emer.php
@@ -1,7 +1,7 @@
 <?php 
 
 $fac = $_REQUEST['f'];
-$dir = "var/www/html/10000/";
+$dir = "/var/www/html/10000/";
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 $handler = mysqli_query($datos, "SELECT USR_ID FROM USERS WHERE USR_RANGE = 'admin' OR USR_RANGE = 'sadmin'");
@@ -10,9 +10,9 @@ while ($file = mysqli_fetch_row($handler)){
 	mkdir($dir . $file[0] . "/");
 	mkdir($dir . $file[0] . "_in/");
 	mkdir($dir . $file[0] . "_alt/");
-	chmod($dir . $file[0] . "/", 0777, true);
-	chmod($dir . $file[0] . "_in/", 0777, true);
-	chmod($dir . $file[0] . "_alt/", 0777, true);
+	chmod($dir . $file[0] . "/", 0777);
+	chmod($dir . $file[0] . "_in/", 0777);
+	chmod($dir . $file[0] . "_alt/", 0777);
 
 }
 
