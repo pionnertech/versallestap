590c7a27711a0596f05a00de72d7def2b5a347d6
pionnertch <elaion256@outlook.com>
2015-04-21 15:03:45 -0300

    hit

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -2334,8 +2334,9 @@ function changeListener(){
                  if(parseInt(packets[0]) !== 0 && packets[0] !== "" ){
                        showAlert(packets[2], "pro", packets[0]);
                        //si es de tipo externo ==*.*==
+                       // 21/04/2015 cambie de .st a .iss_id por indices para externos
                             if(parseInt(packets[9]) == 0){
-                                indice = $("input.st[value=" + packets[5] + "]").index(".st");
+                                indice = $("input.iss_id[value=" + packets[5] + "]").index(".st");
                                 var kilo = "ext";
                                     } else {
                                 indice = $("input.hi-int-id[value=" + packets[5] + "]").index(".hi-int-id");
