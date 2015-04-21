fc90b9d7ebe15857bded15bb3f6984acd6029ceb
pionnertch <elaion256@outlook.com>
2015-04-20 21:57:25 -0300

    new

--- backend/netstat.php
+++ backend/netstat.php
@@ -1,8 +1,10 @@
 <?php 
 
-
-
  $rest = shell_exec("netstat -t");
-echo $rest;
+  
+
+ preg_match_all('/\b\d{1,3}\-\d{1,3}\-\d{1,3}\-\d{1,3}\b/g', $rest , $match_array)
+
+print_r($match_array);
 
 ?>
