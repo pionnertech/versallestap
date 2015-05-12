e332273e584bb84ed9b0f7a1340d50e82d190cd7
pionnertch <elaion256@outlook.com>
2015-05-11 22:04:32 -0300

    ersaer

--- scripts/script-int.js
+++ scripts/script-int.js
@@ -2,7 +2,7 @@ $(function(){
 
     var ul = $('#upload2 ul');
 
-    $('#drop a').click(function(){
+    $('#drop2 a').click(function(){
         // Simulate a click on the file input button
         // to show the file browser dialog
         $(this).parent().find('input').click();
