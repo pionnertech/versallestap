18208b010d4c0bb514acd32a8fa149b205714794
pionnertch <elaion256@outlook.com>
2015-05-13 15:48:55 -0300

    plosish

--- backend/files_back_to_admin.php
+++ backend/files_back_to_admin.php
@@ -7,6 +7,7 @@ $kind    = $_REQUEST['kind'];
 $current = $_REQUEST['current'];
 $bingo   = false;
 
+
 if($current !== $user){
   $bingo = true;
 }
@@ -15,7 +16,7 @@ $datos = $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000")
 
 $query =  mysqli_fetch_assoc(mysqli_query($datos, "SELECT STSK_ISS_ID FROM SUBTASKS WHERE STSK_ID = " . $stsk ));
 
-$userId = mysqli_query($datos, "SELECT STSK_CHARGE_USR FROM SUBTASKS WHERE STSK_ISS_ID = " . $query['STSK_ISS_ID']);
+$userId = mysqli_query($datos, "SELECT STSK_CHARGE_USR FROM SUBTASKS WHERE (STSK_ISS_ID = " . $query['STSK_ISS_ID'] . " AND STSK_FAC_CODE = " . $fac . ");");
 
 if($kind == 0 || $kind == "0"){
  while( $fila = mysqli_fetch_row($userId) ){
@@ -52,7 +53,7 @@ if($kind == 0 || $kind == "0"){
          if($bingo == true){
             $outcome .= "../". $fac . "/" . $fila[0] ."_alt/" . $files . "|";
           } else {
-              if($user == $fila[0] ){
+              if((int)$user == (int)$fila[0] ){
                 $outcome .= "../". $fac . "/" . $fila[0] ."_alt/" . $files . "|";
             } else {
               continue;
