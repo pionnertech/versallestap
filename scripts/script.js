4b551fc8198675f22fabda8b8504fe862e6d9ac6
pionnertch <elaion256@outlook.com>
2015-05-18 21:03:59 -0300

    grafico

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,8 @@ $(function(){
             var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
                 ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span class="fa fa-times af" onclick="rewind(this)"></span></li>');
 
+            
+
             // Append the file name and file size
             tpl.find('p').text(data.files[0].name)
                          .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
