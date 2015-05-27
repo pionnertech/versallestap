f21e8297e060f8cdfb169315c1e7fb91d6fd9488
pionnertch <elaion256@outlook.com>
2015-05-27 15:43:32 -0300

    adjust

--- backend/json_searchengine.php
+++ backend/json_searchengine.php
@@ -5,10 +5,13 @@ $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 $query = mysqli_query($datos, "SELECT STSK_DESCRIP, STSK_START_DATE, STSK_FINISH_DATE FROM SUBTASKS WHERE (STSK_FAC_CODE = 10000 AND STSK_TYPE =0)");
 
 $i = 0;
+
 $count = mysqli_num_rows($query);
 
-echo "[";
 
+echo "<script>
+ var subject = [";
+/*
 while ($fila = mysqli_fetch_row($query)){
 	echo "{ \"f1\":\"" . $fila[0] . "\",";
 	echo "\"f2\":\"" . $fila[1] . "\",";
@@ -21,3 +24,17 @@ while ($fila = mysqli_fetch_row($query)){
 }
 
 echo "]";
+*/
+
+while ($fila = mysqli_fetch_row($query)){
+	echo str_replace(","," ",$fila[0]);
+
+		 if($i < $count -1){
+        echo ",";
+	 }
+	 $i = $i + 1;
+}
+
+echo "] </script>";
+
+?>
