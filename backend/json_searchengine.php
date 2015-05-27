328c6ca2155f3505a0e18b266475942f64a2da92
pionnertch <elaion256@outlook.com>
2015-05-27 14:39:30 -0300

    adjust

--- backend/json_searchengine.php
+++ backend/json_searchengine.php
@@ -1,10 +1,11 @@
-<?php 
+<?php header('Content-Type: text/html; charset=utf-8');
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-$query = mysqli_query($datos, "SELECT STSK_DESCRIPT, STSK_START_DATE, STSK_FINISH_DATE FROM SUBTASKS WHERE (STSK_FAC_CODE = 10000)");
+$query = mysqli_query($datos, "SELECT STSK_DESCRIP, STSK_START_DATE, STSK_FINISH_DATE FROM SUBTASKS WHERE STSK_FAC_CODE = 10000");
 
 $i = 0;
+$count = mysqli_num_rows($query);
 
 echo "[";
 
@@ -12,8 +13,8 @@ while ($fila = mysqli_fetch_row($query)){
 	echo "{ \"f1\":\"" . $fila[0] . "\",";
 	echo "\"f2\":\"" . $fila[1] . "\",";
 	echo "\"f2\":\"" . $fila[1] . "\"}";
-	
-	 if($i < count($fila)-1){
+
+	 if($i < $count -1){
         echo ",";
 	 }
 }
