c11412dbdc647f9ee007f123ce1920636f13be66
pionnertch <elaion256@outlook.com>
2015-06-02 21:16:53 -0300

    photos

--- backend/coverflow.php
+++ backend/coverflow.php
@@ -7,7 +7,7 @@ if(isset($_POST["U"]))
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-$query = "SELECT USR_ID, USR_NAME, USR_SURNAME, USR_FACILITY, USR_RANGE, USR_DEPT, USR_CHARGE FROM USERS WHERE (USR_NICK = '" . $_POST["U"] . "' AND USR_PASS = '" . $_POST['P'] . "')";
+$query = "SELECT A.USR_ID, A.USR_NAME, A.USR_SURNAME, A.USR_FACILITY, A.USR_RANGE, A.USR_DEPT, A.USR_CHARGE, B.FAC_NAME FROM USERS A INNER JOIN FACILITY B ON(A.USR_FACILITY = B.FAC_CODE) WHERE (USR_NICK = '" . $_POST["U"] . "' AND USR_PASS = '" . $_POST['P'] . "')";
 
 $outcome = mysqli_fetch_assoc(mysqli_query($datos, $query));
 
@@ -20,24 +20,26 @@ if (!$outcome)
     $_SESSION["TxtCode"] = "";
     $_SESSION["TxtRange"] = "";
     $_SESSION["TxtPosition"] = "";
+    $_SESSION["TxtFacName"] = "";
 
     session_destroy();
 
 echo "<script language='javascript'>window.location='../index.php?t=1'; </script>";
-
 
 } 
 
 
 else { 
 
- $_SESSION["TxtUser"] = $outcome['USR_NAME'];
- $_SESSION["TxtPass"] = $outcome['USR_SURNAME'];
+ $_SESSION["TxtUser"]     = $outcome['USR_NAME'];
+ $_SESSION["TxtPass"]     = $outcome['USR_SURNAME'];
  $_SESSION["TxtFacility"] = $outcome['USR_FACILITY'];
- $_SESSION["TxtCode"] = $outcome['USR_ID'];
- $_SESSION["TxtRange"] = $outcome['USR_RANGE'];
- $_SESSION["TxtDept"] = $outcome['USR_DEPT'];
+ $_SESSION["TxtCode"]     = $outcome['USR_ID'];
+ $_SESSION["TxtRange"]    = $outcome['USR_RANGE'];
+ $_SESSION["TxtDept"]     = $outcome['USR_DEPT'];
  $_SESSION["TxtPosition"] = $outcome['USR_CHARGE'];
+ $_SESSION["TxtFacName"]  = $outcome['FAC_NAME'];
+
 
 switch ($outcome['USR_RANGE']) {
 	
