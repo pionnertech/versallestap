3d5ae856956a6fc6755bf81c4ca186ebab8b18d9
pionnertch <elaion256@outlook.com>
2015-06-02 21:27:31 -0300

    photos

--- Admin/other-user-profile.php
+++ Admin/other-user-profile.php
@@ -9,7 +9,7 @@ $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 $Query_name = mysqli_query($datos, "SELECT FAC_NAME FROM FACILITY WHERE FAC_CODE = " . $_SESSION['TxtCode']);
 
-$Query_team       = mysqli_query($datos, "SELECT USR_ID, USR_NAME, USR_SURNAME FROM USERS WHERE (USR_FACILITY = " . $_SESSION['TxtFacility'] . " AND USR_RANGE = 'back-user' AND USR_DEPT = '" .  $_SESSION["TxtDept"] . "');");
+$Query_team       = mysqli_query($datos, "SELECT USR_ID, USR_NAME, USR_SURNAME, USR_MAIL FROM USERS WHERE (USR_FACILITY = " . $_SESSION['TxtFacility'] . " AND USR_RANGE = 'back-user' AND USR_DEPT = '" .  $_SESSION["TxtDept"] . "');");
 $Query_subtask    = mysqli_query($datos, "SELECT A.STSK_ID, A.STSK_ISS_ID, A.STSK_DESCRIP, B.EST_DESCRIPT, A.STSK_FINISH_DATE, B.EST_COLOR, A.STSK_PROGRESS, A.STSK_LOCK FROM SUBTASKS A INNER JOIN EST B ON(B.EST_CODE = A.STSK_STATE) WHERE (STSK_TYPE = 0 AND STSK_CHARGE_USR = " . $_SESSION['TxtCode'] . " AND STSK_FAC_CODE = " . $_SESSION['TxtFacility'] . " ) ORDER BY STSK_FINISH_DATE " );
 $Query_alerts_ext = mysqli_query($datos, "SELECT COUNT(STSK_ID), STSK_STATE FROM SUBTASKS WHERE (STSK_CHARGE_USR = " . $_SESSION['TxtCode'] . " AND STSK_TYPE = 0 AND STSK_FAC_CODE = " . $_SESSION['TxtFacility'] . ") GROUP BY STSK_STATE");
 $Query_alerts_int = mysqli_query($datos, "SELECT COUNT(STSK_ID), STSK_STATE FROM SUBTASKS WHERE (STSK_FAC_CODE = " . $_SESSION['TxtFacility'] . " AND STSK_MAIN_USR = " . $_SESSION['TxtCode'] . " AND STSK_CHARGE_USR = STSK_MAIN_USR AND STSK_TYPE = 1) GROUP BY STSK_STATE");
@@ -305,9 +305,9 @@ $query_incoming = mysqli_query($datos, "SELECT A.STSK_ID, A.STSK_MAIN_USR, CONCA
                                                             <p>
                                                                 <small class="muted">Serviu</small></p>
                                                             <div class="media-option btn-group shaded-icon">
-                                                                <button class="btn btn-small">
+                                                                <a class="btn btn-small" href="mailto:<? echo $fila_per[3] ?>">
                                                                     <i class="icon-envelope"></i>
-                                                                </button>
+                                                                </a>
                                                                 <button class="btn btn-small">
                                                                     <i class="icon-share-alt"></i>
                                                                 </button>
@@ -380,9 +380,9 @@ $handler = mysqli_query($datos, $matrix);
                                                             <p>
                                                                 <small class="muted">Serviu</small></p>
                                                             <div class="media-option btn-group shaded-icon">
-                                                                <button class="btn btn-small">
+                                                                <a class="btn btn-small" href="mailto:<? echo $fila_per2[3] ?>">
                                                                     <i class="icon-envelope"></i>
-                                                                </button>
+                                                                </a>
                                                                 <button class="btn btn-small">
                                                                     <i class="icon-share-alt"></i>
                                                                 </button>
