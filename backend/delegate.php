7d22158002616f77414e4eb26752121ad16248f5
pionnertch <elaion256@outlook.com>
2015-06-10 03:35:39 -0300

    switch

--- backend/delegate.php
+++ backend/delegate.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $fac = $_GET['fac'];
 $usr_id = $_GET['usr_id'];
 $rut = $_GET['rut'];
@@ -19,10 +18,8 @@ mysqli_query($datos, "UPDATE ISSUES SET ISS_FINISH_DATE = '" . $dataF . "' WHERE
 
 $iss_ticket = mysql_fetch_assoc(mysqli_query($datos, "SELECT ISS_TICKET FROM ISSUES WHERE ISS_ID = " . $iss_id));
 
-
 $query_insert = "INSERT INTO SUBTASKS(STSK_ISS_ID, STSK_DESCRIP, STSK_CHARGE_USR , STSK_STATE, STSK_FINISH_DATE , STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE, STSK_PROGRESS, STSK_LOCK, STSK_TYPE, STSK_TICKET) ";
 $query_insert .= "VALUES (" . $iss_id  . " , '" . $msg . "', '" . $usr_id  . "', 2, '" . $dataF . "','" . $dataS . "', '" . $usr_id  . "', " . $fac  . ", 0 , 0, 0, '" . $iss_ticket['ISS_TICKET'] . "')";
-
 
 if(!mysqli_query($datos, $query_insert)){
 
