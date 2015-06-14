7b6dc88d24fbc87c454e4a246d7e4684e02b5ce7
pionnertch <elaion256@outlook.com>
2015-06-14 14:32:09 -0300

    new Task

--- backend/unlock.php
+++ backend/unlock.php
@@ -7,7 +7,7 @@ $type= $_GET['type'];
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-if(!mysqli_query($datos,"UPDATE SUBTASKS SET STSK_LOCK = 1 WHERE (STSK_ID = " . $stsk_id . " AND STSK_FAC_CODE = " . $fac . " AND STSK_TYPE = " . $type . ")" )){
+if(!mysqli_query($datos,"UPDATE SUBTASKS SET STSK_LOCK = 1 WHERE (STSK_ISS_ID  = " . $iss_id   . " AND STSK_FAC_CODE = " . $fac . " AND STSK_TYPE = " . $type . ")" )){
 	echo 0;
 } else {
 if(isset($iss_id)){
