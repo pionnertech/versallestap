bf57a9de8a86ba89e39580cb178904aed256fb2f
pionnertch <elaion256@outlook.com>
2015-06-15 08:54:21 -0300

    d3 and switchfiles-new

---
+++ backend/switchfiles-new.php
@@ -0,0 +1,80 @@
+<?php
+
+
+$fac = $_GET['fac'];
+$file_name = $_GET['file_name'];
+$main_usr_id = $_GET['main_usr_id'];
+$charge_usr_id = $_GET['charge_usr_id'];
+
+//first check if exist
+$dir = "/var/www/html/" . $fac . "/";
+
+$datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
+
+$dept = mysqli_fetch_assoc(mysqli_query($datos, "SELECT USR_DEPT FROM USERS WHERE (USR_ID = " . $main_usr_id . " AND USR_FACILITY= " . $fac . ")"));
+
+if($charge_usr_id == "Mi Departamento"){
+   
+   $team_ids = mysqli_query($datos, "SELECT USR_ID FROM USERS WHERE (USR_DEPT = " . $dept['USR_DEPT'] . " AND USR_FACILITY = " . $fac . " AND USR_RANGE = 'back-user')");
+
+       while($fila = mysqli_fetch_row($team_ids)){
+                 
+                 if(!is_dir($dir . $fila[0] . "/")){
+	                    mkdir($dir . $fila[0] . "/", 0775, true);
+                    }
+                 if(!is_file($dir . $main_usr_id . "/" . $file_name)){
+	                    echo "no file!";
+	                    exit;
+                    }
+                 copy($dir . $main_usr_id . "/" . $file_name, $dir . $fila[0] . "/" . $file_name );
+            }
+
+} else {
+
+
+$team = explode(",", $charge_usr_id);
+
+for($i=0; $i < count($team); $i++){
+
+ $usr_id_q = mysqli_fetch_assoc(mysqli_query($datos, "SELECT USR_ID FROM USERS WHERE (CONCAT(USR_NAME, ' ' , USR_SURNAME) = '" . strtoupper($team[$i]) . "' AND USR_FACILITY = " . $fac .")"));
+
+                  if(!is_dir($dir . $usr_id_q['USR_ID'] . "/")){
+	                    mkdir($dir . $fila[0] . "/", 0775, true);
+                    }
+                 if(!is_file($dir . $main_usr_id . "/" . $file_name)){
+	                    echo "no file!";
+	                    exit;
+                    }
+
+             copy($dir . $main_usr_id . "/" . $file_name, $dir . $usr_id_q['USR_ID'] . "/" . $file_name );
+
+}
+
+}
+
+
+
+
+
+/*
+if(!is_dir($dir . $charge_usr_id . "/")){
+	mkdir($dir . $charge_usr_id . "/", 0775, true);
+}
+*/
+/*
+if(!is_file($dir . $main_usr_id . "/" . $file_name)){
+	echo "no file!";
+	exit;
+}
+
+
+ if(copy($dir . $main_usr_id . "/" . $file_name, $dir . $charge_usr_id . "/" . $file_name )){
+ 	echo "was copied";
+ 	unlink($dir . $main_usr_id . "/" . $file_name);
+ } else {
+ 	echo $dir . $main_usr_id . "/" . $file_name  . "<br />";
+ 	echo $dir . $main_usr_id . "/" . $file_name;
+ }
++/
+
+?>
