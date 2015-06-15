ce51eeac6e8242062dbfa399b10deb05c9ee6632
pionnertch <elaion256@outlook.com>
2015-06-15 09:15:53 -0300

    d3 and switchfiles-new

--- backend/stsk-del-new.php
+++ backend/stsk-del-new.php
@@ -44,22 +44,5 @@ if($user == "Mi Departamento"){
 
 
 
-/*
-$query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_STATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE, STSK_PROGRESS, STSK_TYPE) ";
-$query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', '" . $user . "', '" . $fechaF . "', 1 ,  '" . $startD . "' , '" . $muser . "', " . $fac . ", 0, 0)";
-
-if(!mysqli_query($datos, $query)){
-
-echo 0;
-
-} else {
-
- $handler = mysqli_query($datos, "SELECT CONCAT(B.USR_NAME, '', B.USR_SURNAME) AS NAME FROM SUBTASKS A INNER JOIN USERS B ON(B.USR_ID = A.STSK_CHARGE_USR) WHERE (STSK_ISS_ID = " . $iss_id . " AND STSK_CHARGE_USR != STSK_MAIN_USR AND STSK_FAC_CODE = " . $fac . ")");
- while ($fila1v = mysqli_fetch_row($handler)){
- 	echo str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($fila1v['NAME']))));
- }
-
-*/
-
 
 ?>
