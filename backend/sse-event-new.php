09a120d35c282691eb160e504248e7fa9b7755f9
pionnertch <elaion256@outlook.com>
2015-07-06 19:17:22 -0300

    ribbon

--- backend/sse-event-new.php
+++ backend/sse-event-new.php
@@ -7,7 +7,7 @@ header('Cache-Control: no-cache');
 
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-$str_query = "SELECT A.STSK_DESCRIP, A.STSK_ID, B.ISS_DESCRIP, B.ISS_ID , CONCAT(C.CTZ_NAMES, ' ' , C.CTZ_SURNAME1, ' ' , C.CTZ_SURNAME2 ) AS NAME, C.CTZ_TEL, C.CTZ_ADDRESS, A.STSK_FINISH_DATE, C.CTZ_GEOLOC " .
+$str_query = "SELECT A.STSK_DESCRIP, A.STSK_ID, B.ISS_DESCRIP, B.ISS_ID , CONCAT(C.CTZ_NAMES, ' ' , C.CTZ_SURNAME1, ' ' , C.CTZ_SURNAME2 ) AS NAME, C.CTZ_TEL, C.CTZ_ADDRESS, A.STSK_FINISH_DATE, A.STSK_TICKET, C.CTZ_GEOLOC " .
 "FROM `SUBTASKS` A " .
 "INNER JOIN `ISSUES` B ON(A.STSK_ISS_ID = B.ISS_ID) " .
 "INNER JOIN `CITIZENS` C ON(B.ISS_CTZ = C.CTZ_RUT ) WHERE STSK_CHARGE_USR = " . $a . " ORDER BY STSK_ID DESC LIMIT 1";
@@ -27,7 +27,9 @@ echo "data:" . $manu['NAME'] . "\n";
 echo "data:" . $manu['CTZ_TEL'] . "\n";
 echo "data:" . $manu['CTZ_ADDRESS'] . "\n";
 echo "data:" . str_replace("-","/" , date("d/m/Y", strtotime(substr($manu['STSK_FINISH_DATE'], 0, 10)))) . "\n";
-echo "data:" . $manu['CTZ_GEOLOC'] . "\n\n";
+echo "data:" . $manu['CTZ_GEOLOC'] . "\n";
+echo "data:" . $manu['STSK_TICKET'] . "\n\n";
+
 
 ob_end_flush();
 flush();
