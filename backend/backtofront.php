0a27ccf5eb7a4040635d78bddb4b1adbfe6f901b
pionnertech <elaion256@outlook.com>
2015-07-09 18:39:59 -0300

    try back to front

--- backend/backtofront.php
+++ backend/backtofront.php
@@ -7,10 +7,10 @@ $iss  = $_GET['iss'];
 
 $dir = "/var/www/html/" . $fac . "/" . $usr . "_in/";
 
-$file = preg_replace('/[0-9]+/' , $iss , $p);
+$file = preg_replace('/[0-9]+/' , "[" . $iss . "]" , $p);
 
 if(copy($dir . $p, "/var/www/html/" . $fac . "/reply/" . $file)){
-	echo "copiado!";
+	echo "/var/www/html/" . $fac . "/reply/" . $file;
 } else {
 	echo "no copiado";
 }
