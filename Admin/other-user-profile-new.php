d73c255404f8be576ac58fe17b286f51cff1df31
pionnertech <elaion256@outlook.com>
2015-07-09 19:25:49 -0300

    plast

--- Admin/other-user-profile-new.php
+++ Admin/other-user-profile-new.php
@@ -2582,9 +2582,7 @@ function thirdPulling(){
 
                     var delta = data.split("\n");
                         for(i=0; i < delta.length ; i++){
-                            var alpha = delta[0].split("|");
-                            console.info("console.info delta 1 :" + delta[1]);
-                              console.info("console.info delta 0 :" + delta[0]);
+                            var alpha = delta[1].split("|");
                             if(alpha[1] !== undefined){
                              if(alpha !== aa_ii){
                               firstTask(alpha[0], alpha[2], alpha[9] , alpha[3], alpha[6], 0, alpha[1], 0, alpha[10]);
