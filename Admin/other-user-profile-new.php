ab232815eb0bbf6047ea435ff6473fe1fd2dcc2a
pionnertech <elaion256@outlook.com>
2015-07-22 11:29:23 -0300

    fixed admin

--- Admin/other-user-profile-new.php
+++ Admin/other-user-profile-new.php
@@ -4515,7 +4515,7 @@ console.info("remoteUser:" + remoteUser + " st_ii :" + st_ii + " ii_iss : " + ii
    $(".span2").slider('setValue', percent);
 $("#back-own").data("val", 1);
 $("#int-require").removeClass('active in');
-$("#set-pro-int").addClass('active in')
+$("#set-pro-int").addClass('active in');
 
 }  else {
 
