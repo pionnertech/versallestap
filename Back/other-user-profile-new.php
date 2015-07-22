86e4f2b96eda7060962b12315b176d912cfb24e2
pionnertech <elaion256@outlook.com>
2015-07-22 11:19:00 -0300

    web socket try

--- Back/other-user-profile-new.php
+++ Back/other-user-profile-new.php
@@ -694,8 +694,8 @@ $trf_hand = mysqli_query($datos, $str_query_trf);
                                           $file_extension = "";
 
                                            while (false !== ($archivos2 = readdir($handler))){
-echo "<script>console.info('" . preg_match_all("/_\[" . $fila_int[1] . "\]_/", $archivos2) . " ---- " . $fila_int[1] . " ---  " . "../" . $_SESSION['TxtFacility'] . "/" . $boss['BOSS'] . "_alt/')</script>";
-                                         if(preg_match_all("/_\[" . $fila_int[1] . "\]_/", $archivos2) == 1){
+echo "<script>console.info('" . preg_match_all("/_\[" . $fila_int[0] . "\]_/", $archivos2) . " ---- " . $fila_int[1] . " ---  " . "../" . $_SESSION['TxtFacility'] . "/" . $boss['BOSS'] . "_alt/')</script>";
+                                         if(preg_match_all("/_\[" . $fila_int[0] . "\]_/", $archivos2) == 1){
                                              
                                              $extension = substr($archivos2, -3);
                                               $cor = "";
