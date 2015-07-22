c93b0404a99bbace71e0925646d571197a005722
pionnertech <elaion256@outlook.com>
2015-07-22 11:07:47 -0300

    mejoras layout internal ii

--- Sadmin/other-user-profile-new.php
+++ Sadmin/other-user-profile-new.php
@@ -1398,7 +1398,7 @@ $spec_tem = mysqli_query($datos, "SELECT CONCAT(A.USR_NAME , ' ',  A.USR_SURNAME
                                                                                                                                        
                                                                     <div class="coll-int" style="width: 100%">
 
-             <?  $part = mysqli_query($datos, "SELECT A.STSK_CHARGE_USR, CONCAT(B.USR_NAME, ' ', B.USR_SURNAME), A.STSK_ID, A.STSK_PROGRESS, A.STSK_ISS_ID FROM SUBTASKS A INNER JOIN USERS B ON(B.USR_ID = A.STSK_CHARGE_USR) WHERE (STSK_TYPE= 1 AND STSK_ISS_ID =" . ((int)$fila5[11]+1) . " AND STSK_CHARGE_USR <> STSK_MAIN_USR)"); 
+             <?  $part = mysqli_query($datos, "SELECT A.STSK_CHARGE_USR, CONCAT(B.USR_NAME, ' ', B.USR_SURNAME), A.STSK_ID, A.STSK_PROGRESS, A.STSK_ISS_ID FROM SUBTASKS A INNER JOIN USERS B ON(B.USR_ID = A.STSK_CHARGE_USR) WHERE (STSK_TYPE= 1 AND STSK_ISS_ID =" . ((int)$fila5[11]+1) . " AND STSK_CHARGE_USR != STSK_MAIN_USR)"); 
                                while($prt = mysqli_fetch_row($part)){
              ?>
                                                                         <a data-per="<? echo $prt[3] ?>" class="hovertip" title="<? printf(str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($prt[1]))))) ?>">
@@ -1427,8 +1427,8 @@ $spec_tem = mysqli_query($datos, "SELECT CONCAT(A.USR_NAME , ' ',  A.USR_SURNAME
                                           $file_extension = "";
 
                                            while (false !== ($archivos2 = readdir($handler2))){
-echo "<script>console.info('tester : " . $archivos2 . "' + ' / ' + '" . preg_match_all("/_\[" . $fint[4]. "\]_/", $archivos2) . "' + '/' + '" . $fint[4] . "' )</script>";
-                                         if(preg_match_all("/_\[" . $fint[4] . "\]_/", $archivos2) == 1){
+echo "<script>console.info('tester : " . $archivos2 . "' + ' / ' + '" . preg_match_all("/_\[" . ($fint[4]+1). "\]_/", $archivos2) . "' + '/' + '" . ($fint[4] + 1). "' )</script>";
+                                         if(preg_match_all("/_\[" . ($fint[4]+1) . "\]_/", $archivos2) == 1){
 
                                              $extension = substr($archivos2, -3);
                                               $cor = "";
