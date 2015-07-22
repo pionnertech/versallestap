a1d645f9e01a6091f3bb0d7aff7e3173758cb09c
pionnertech <elaion256@outlook.com>
2015-07-22 11:08:55 -0300

    mejoras layout internal ii

--- backend/upload_int.php
+++ backend/upload_int.php
@@ -1,12 +1,20 @@
 <?php
 
+
 $fac     = $_REQUEST['fac_id'];
 $user    = $_REQUEST['user'];
 $keyfile = $_REQUEST['keyfile'];
+	$dir = "/var/www/html/" . $fac . "/_tmp/";
+	$iss_id = $keyfile;
+if(!is_dir($dir)){
+	mkdir($dir, 0775, true);
+}
+$extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+if(move_uploaded_file($_FILES['file']['tmp_name'] , $dir . basename($_FILES['file']['name'], "." . strtolower($extension)) . "_[" . $iss_id . "]_." . $extension  )) {
+	echo $dir . basename($_FILES['file']['name'], "." . strtolower($extension)) . "_[" . $iss_id . "]_." . $extension ;
+}
 
-$iss_id = $keyfile;
-
-
+/*
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 
@@ -45,7 +53,7 @@ switch ($user) {
 	 }
 
 
-
+*/
 
 
 ?>
