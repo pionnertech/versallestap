e83684aedec69207fdb21b3599387e604d84bd11
pionnertech <elaion256@outlook.com>
2015-07-22 11:00:58 -0300

    mejoras layout internal ii

--- backend/delegate_internal-new.php
+++ backend/delegate_internal-new.php
@@ -157,7 +157,7 @@ $uteam = mysqli_query($datos, "SELECT A.USR_ID, B.STSK_ID FROM USERS A INNER JOI
               while($uteams = mysqli_fetch_row($uteam)){
 
                 if(copy("/var/www/html/" . $fac . "/_tmp/" . $files ,  $dir . $uteams[0] . "_alt/" . basename(str_replace("_[" . $keyfile . "]_" , "", $files), "." . strtolower($extension)) . "_[" . $uteams[1] . "]_." . $extension)){
-                        echo $dir . $uteams[0] . "_alt/" . basename(str_replace("_[" . $keyfile . "]_" , "", $files), "." . strtolower($extension)) . "_[" . $uteams[1] . "]_." . $extension);
+                        echo $dir . $uteams[0] . "_alt/" . basename(str_replace("_[" . $keyfile . "]_" , "", $files), "." . strtolower($extension)) . "_[" . $uteams[1] . "]_." . $extension;
                   } else {
                       echo "/var/www/html/" . $fac . "/_tmp/" . $files;
                   }
