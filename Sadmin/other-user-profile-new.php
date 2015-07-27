6d834aa10ac7d0f1ef3fa663e70d3d7ae2615537
pionnertech <elaion256@outlook.com>
2015-07-26 22:16:40 -0300

    fixing in sadmin

--- Sadmin/other-user-profile-new.php
+++ Sadmin/other-user-profile-new.php
@@ -4565,12 +4565,13 @@ if(kind == 1){
       div5.style.width = "80%";
 
   var nname_pre = $("#D-drop").data("dfil");
+
   console.info("-")
   console.info( $("#D-drop").data("dfil"));
   console.log(nname_pre);
   console.info("-");
-  
-  if(nname_pre !== undefined || nname_pre !== ""){
+
+  if($("#D-drop").data("dfil") !== undefined){
 
   var nname = nname_pre.split("|");
   var setClass = "";
