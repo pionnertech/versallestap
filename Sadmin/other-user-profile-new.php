827d0562e330f7a9c3b853026daa38c09e00684b
pionnertech <elaion256@outlook.com>
2015-07-26 22:45:24 -0300

    all

--- Sadmin/other-user-profile-new.php
+++ Sadmin/other-user-profile-new.php
@@ -3448,14 +3448,9 @@ function changeListener(){
                             if(packets.length == 12){
                               nest = packets[11];
                             }
-                console.info("indice = " + nest + " packet[5] = " + packets[5]); 
-
-
-
             updateProgress(packets[2], packets[3], packets[6], packets[4], packets[1], packets[0], indice, packets[5], packets[9], nest, packets[10]);
                           //aqui si es de tipo externo \./\./
-                             console.info(indice);
-                             console.info("progreso del usuario : " + packets[10]);
+                             
                         if(parseInt(packets[10]) >= 99.5){
                           if(packets[9] == 0){
 
