771ef052b83fd782e5e826a062a76c982fb2cedb
pionnertech <elaion256@outlook.com>
2015-07-26 22:50:06 -0300

    test super listener

--- backend/filepack.php
+++ backend/filepack.php
@@ -5,20 +5,23 @@ $fac    = $_GET['fac'];
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 $it = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " )");
-$ad = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " AND USR_RANGE ='admin' )")
+$ad = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " AND USR_RANGE ='admin' )");
 
 
 while($fila = mysqli_fetch_row($ad)){
-    $handle = opendir("/var/www/html/" . $fac . "/" . $fila[1] . "_alt/");
-        while (false !== ($file = readdir($handle))){
-        	while ($as = mysqli_fetch_row($it)){
-            echo $file . "  - -- " . preg_match_all("/_\[" . $fila[0] . "\]_/", $file) . " --- " . "preg_match_all(/_\[" . $fila[0] . "\]_/," . $file . ")";
-	        if( preg_match_all("/_\[" . $as[0] . "\]_/", $file) == 1){
-             echo $file . ",";
-	        }
-        	}
 
-    }
+    $handle = opendir("/var/www/html/" . $fac . "/" . $fila[1] . "_alt/");
+
+        while (false !== ($file = readdir($handle))){
+
+        	   while ($as = mysqli_fetch_row($it)){
+                         echo $file . "  - -- " . preg_match_all("/_\[" . $fila[0] . "\]_/", $file) . " --- " . "preg_match_all(/_\[" . $fila[0] . "\]_/," . $file . ")";
+	                     if( preg_match_all("/_\[" . $as[0] . "\]_/", $file) == 1){
+                              echo $file . ",";
+	                        }
+        	          }
+
+             }
 }
 
 
