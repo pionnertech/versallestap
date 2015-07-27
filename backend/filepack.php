20bbd6f01aee884318cf86431109effb8ee5b30d
pionnertech <elaion256@outlook.com>
2015-07-26 22:24:16 -0300

    test super listener

--- backend/filepack.php
+++ backend/filepack.php
@@ -4,8 +4,9 @@ $ticket = $_GET['ticket'];
 $fac    = $_GET['fac'];
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
-$it = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> 'sadmin' AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " )");
-$ad = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> 'sadmin' AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " AND USR_RANGE ='admin' )");
+$it = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1  AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " AND USR_RANGE ='back-user' )");
+$ad = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> 'sadmin' AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " )");
+
 
 
 while($fila = mysqli_fetch_row($ad)){
@@ -25,10 +26,23 @@ while($fila = mysqli_fetch_row($ad)){
              closedir($handle);
 }
 
+ mysqli_data_seek($it, 0);
+ mysqli_data_seek($ad, 0);
 
+    $handle = opendir("/var/www/html/" . $fac . "/1_alt/");
+ while ($as = mysqli_fetch_row($it)){
+        while (false !== ($file = readdir($handle))){
+                        // echo $file . "  - -- " . preg_match_all("/_\[" . $as[0] . "\]_/", $file) . " --- " . "preg_match_all(/_\[" . $as[0] . "\]_/," . $file . ")";
+	                     if( preg_match_all("/_\[" . $as[0] . "\]_/", $file) == 1){
+                              echo $file . ",";
+	                        }
+        	          
+               
+             }
 
+           }
+}
 
-
-
+ mysqli_data_seek($it, 0);
 
  ?>
