c53924fe03986e31596b27339703871eed48f36a
pionnertech <elaion256@outlook.com>
2015-07-26 22:19:15 -0300

    theather

--- backend/time-beta.php
+++ backend/time-beta.php
@@ -27,6 +27,11 @@ switch ($sel['STSK_TYPE']) {
 	   //se saca el promedio y se obtien el valor parcial  
        //  =======  caso back =======//
 
+
+	  // determine if is Finished
+	   if($sel['STSK_PROGRESS'] > 99.9){  $classText = "FINALIZADO" } else {  $classText = "" };
+	   
+
 	   //update the difference , set the traffic and variables
        mysqli_query($datos, "UPDATE SUBTAKS SET STSK_ANCIENT_PRO = STSK_PROGRESS WHERE STSK_ID = " . $sel["STSK_ID"]);
        //get the gral avg
@@ -35,21 +40,24 @@ switch ($sel['STSK_TYPE']) {
        $trf_str = mysqli_fetch_assoc(mysqli_query($datos, "SELECT * FROM TRAFFIC WHERE TRF_STSK_ID = " . $sel['STSK_ID']));
        
                 echo str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($sel['NAME']))));
-                echo "|" .$user['TRF_USER'];
-                echo "|" .$user['TRF_SUBJECT'];
-                echo "|" .$user['TRF_DESCRIPT'];
-                echo "|" .date('d/m/Y', strtotime($user['TRF_ING_DATE']));
-                echo "|" .$user['TRF_STSK_SRC_ID'];
-                echo "|" .$total['AVX'];
-                echo "|" .$classText;
-                echo "|" .$user['TII_STSK_ID']; 
+                echo "|" . $trf_str['TRF_USER'];
+                echo "|" . $trf_str['TRF_SUBJECT'];
+                echo "|" . $trf_str['TRF_DESCRIPT'];
+                echo "|" . date('d/m/Y', strtotime( $trf_str['TRF_ING_DATE']));
+                echo "|" . $trf_str['TRF_STSK_SRC_ID'];
+                echo "|" . $total['AVX'];
+                echo "|" . $classText;
+                echo "|" . $trf_str['TRF_STSK_ID']; 
+                echo "|" . $sel['STSK_PROGRESS'];
+                echo "|" . $sel['STSK_TYPE'];
+
 
 		break;
 	
 	case 1:
 	   //se saca el promedio y se obtien el valor parcial  
        //  =======  caso back =======//
-
+       if($sel['STSK_PROGRESS'] > 99.9){  $classText = "FINALIZADO" } else {  $classText = "" };
 	   //update the difference , set the traffic and variables
        mysqli_query($datos, "UPDATE SUBTAKS SET STSK_ANCIENT_PRO = STSK_PROGRESS WHERE STSK_ID = " . $sel["STSK_ID"]);
        //get the gral avg
@@ -58,16 +66,16 @@ switch ($sel['STSK_TYPE']) {
        $trf_str = mysqli_fetch_assoc(mysqli_query($datos, "SELECT * FROM TRAFFIC_II WHERE TII_STSK_ID = " . $sel['STSK_ID']));
        
                 echo str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($sel['NAME']))));
-                echo "|" .$user['TRF_USER'];
-                echo "|" .$user['TRF_SUBJECT'];
-                echo "|" .$user['TRF_DESCRIPT'];
-                echo "|" .date('d/m/Y', strtotime($user['TRF_ING_DATE']));
-                echo "|" .$user['TRF_STSK_SRC_ID'];
-                echo "|" .$total['AVX'];
-                echo "|" .$classText;
-                echo "|" .$user['TII_STSK_ID']; 
-
-
+                echo "|" . $trf_str['TRF_USER'];
+                echo "|" . $trf_str['TRF_SUBJECT'];
+                echo "|" . $trf_str['TRF_DESCRIPT'];
+                echo "|" . date('d/m/Y', strtotime($trf_str['TRF_ING_DATE']));
+                echo "|" . $user['TRF_STSK_SRC_ID'];
+                echo "|" . $total['AVX'];
+                echo "|" . $classText;
+                echo "|" . $trf_str['TII_STSK_ID']; 
+                echo "|" . $sel['STSK_PROGRESS'];
+                echo "|" . $sel['STSK_TYPE'];
 
 	break;
 
