14639ed40fe0cb389b7e50c9932b756a6a6e372b
pionnertech <elaion256@outlook.com>
2015-07-26 22:53:07 -0300

    theather

--- backend/super-listener-int.php
+++ backend/super-listener-int.php
@@ -62,7 +62,7 @@ echo "|" . $user_out6 . "|";
 }
 
 //iterate every dir for every file by quering by iss
-$it = mysqli_query($datos, "SELECT STSK_ID FROM SUBTASKS WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_ISS_ID = " . $query_assoc['STSK_ISS_ID'] . " AND STSK_FAC_CODE = " . $fac . ")");
+$it = mysqli_query($datos, "SELECT A.STSK_ID FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_TICKET= '" . $query_assoc['STSK_TICKET'] . "' AND STSK_FAC_CODE = " . $fac . " AND USR_RANGE = 'back-user')");
 
 $handle = opendir("/var/www/html/" . $fac . "/" . $handler['PSD_USR'] . "/");
 
