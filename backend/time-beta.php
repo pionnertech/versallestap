2cf317a04c1fd85cd6e61151446f54e71ce343a7
pionnertech <elaion256@outlook.com>
2015-07-26 23:02:51 -0300

    theather

---
+++ backend/time-beta.php
@@ -0,0 +1,81 @@
+<?php
+
+//time nuevo para la escucha de progresos
+
+$usr = $_GET['usr'];
+$fac = $_GET['fac'];
+
+$datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
+//who is listening?
+$ident = mysqli_fetch_assoc(mysqli_query($datos, "SELECT USR_RANGE AS RAN FROM USERS WHERE USR_ID = " . $usr));
+//aqui se detectan los diferenciales de progreso independiente de que origan sean 
+$first = mysqli_query($datos, "SELECT A.STSK_PROGRESS, A.STSK_ANCIENT_PRO, A.STSK_ID, A.STSK_ISS_ID, A.STSK_TYPE, B.USR_RANGE, A.STSK_TICKET, CONCAT(B.USR_NAME, ' ', B.USR_SURNAME) AS NAME FROM SUBTASKS  A INNER JOIN USERS B ON (B.USR_ID = A.STSK_CHARGE_USR) WHERE (STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR = " . $usr . " AND STSK_PROGRESS <> STSK_ANCIENT_PRO ) LIMIT 1 DESC");
+//si no encuentra, salga
+if(mysqli_num_rows($first) < 1){
+     exit; 
+} 
+
+//si encuentra, veamos de que se trata., de quien para quien...
+
+//dicriminemos origen y tipo
+
+$sel = mysqli_fetch_assoc($first);
+
+switch ($sel['STSK_TYPE']) {
+	case 0:
+	   
+	   //se saca el promedio y se obtien el valor parcial  
+       //  =======  caso back =======//
+
+	   //update the difference , set the traffic and variables
+       mysqli_query($datos, "UPDATE SUBTAKS SET STSK_ANCIENT_PRO = STSK_PROGRESS WHERE STSK_ID = " . $sel["STSK_ID"]);
+       //get the gral avg
+	   $total = mysqli_fetch_assoc(mysqli_query($datos, "SELECT ROUND(AVG(CASE STSK_PROGRESS IF IS NULL THEN 0 ELSE STSK_PROGRESS END)) AS AVX FROM SUBTASKS WHERE (STSK_TICKET = '" . $sel['STSK_TICKET'] . "' AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR =  " . $usr . ")" ));
+       //get the INFO about the partial progress
+       $trf_str = mysqli_fetch_assoc(mysqli_query($datos, "SELECT * FROM TRAFFIC WHERE TRF_STSK_ID = " . $sel['STSK_ID']));
+       
+                echo str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($sel['NAME']))));
+                echo "|" .$user['TRF_USER'];
+                echo "|" .$user['TRF_SUBJECT'];
+                echo "|" .$user['TRF_DESCRIPT'];
+                echo "|" .date('d/m/Y', strtotime($user['TRF_ING_DATE']));
+                echo "|" .$user['TRF_STSK_SRC_ID'];
+                echo "|" .$total['AVX'];
+                echo "|" .$classText;
+                echo "|" .$user['TII_STSK_ID']; 
+
+		break;
+	
+	case 1:
+	   //se saca el promedio y se obtien el valor parcial  
+       //  =======  caso back =======//
+
+	   //update the difference , set the traffic and variables
+       mysqli_query($datos, "UPDATE SUBTAKS SET STSK_ANCIENT_PRO = STSK_PROGRESS WHERE STSK_ID = " . $sel["STSK_ID"]);
+       //get the gral avg
+	   $total = mysqli_fetch_assoc(mysqli_query($datos, "SELECT ROUND(AVG(CASE STSK_PROGRESS IF IS NULL THEN 0 ELSE STSK_PROGRESS END)) AS AVX FROM SUBTASKS WHERE (STSK_TICKET = '" . $sel['STSK_TICKET'] . "' AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR =  " . $usr . ")" ));
+       //get the INFO about the partial progress
+       $trf_str = mysqli_fetch_assoc(mysqli_query($datos, "SELECT * FROM TRAFFIC_II WHERE TII_STSK_ID = " . $sel['STSK_ID']));
+       
+                echo str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($sel['NAME']))));
+                echo "|" .$user['TRF_USER'];
+                echo "|" .$user['TRF_SUBJECT'];
+                echo "|" .$user['TRF_DESCRIPT'];
+                echo "|" .date('d/m/Y', strtotime($user['TRF_ING_DATE']));
+                echo "|" .$user['TRF_STSK_SRC_ID'];
+                echo "|" .$total['AVX'];
+                echo "|" .$classText;
+                echo "|" .$user['TII_STSK_ID']; 
+
+
+
+	break;
+
+}
+
+
+
+
+
+
+?>
