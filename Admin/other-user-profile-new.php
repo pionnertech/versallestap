b62627b1a97d8d63db9f855dfa5aeb91193f19de
pionnertech <elaion256@outlook.com>
2015-08-06 22:57:26 -0300

    finger

--- Admin/other-user-profile-new.php
+++ Admin/other-user-profile-new.php
@@ -4754,7 +4754,7 @@ var nname = $("#D-drop").data("dfil").split("|");
 
     if(iin){
 
-    filstr += '<a href="../reply/' + nname[i] + '" title="' + nname[i] +  '" download><i class="fa fa-file-' + setClass + ' fa-2x" style="color:' + cor + '; margin: 0 0.4em"></i></a>';
+    filstr += '<a href="../10000/' + user_id + '/' + nname[i] + '" title="' + nname[i] +  '" download><i class="fa fa-file-' + setClass + ' fa-2x" style="color:' + cor + '; margin: 0 0.4em"></i></a>';
      
      } else{
 
