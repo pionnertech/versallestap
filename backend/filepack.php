94abc336a04466737abb835a96b85a5f35dedcdd
pionnertech <elaion256@outlook.com>
2015-08-09 14:58:37 -0300

    attention

--- backend/filepack.php
+++ backend/filepack.php
@@ -31,6 +31,7 @@ while($fila = mysqli_fetch_row($ad)){
 $reach = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND USR_RANGE = 'admin' AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " AND STSK_RESP = 1)");
 $handle = opendir("/var/www/html/" . $fac . "/1_alt/");
 
+
  while ($as = mysqli_fetch_row($reach)){
 
         while (false !== ($file = readdir($handle))){
