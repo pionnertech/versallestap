ed18c7c9c506523d00f44dde5cc0511a3c23afd0
pionnertech <elaion256@outlook.com>
2015-08-17 14:57:16 -0300

    shutle

--- backend/filepack.php
+++ backend/filepack.php
@@ -7,6 +7,8 @@ $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 $it = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1  AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . " AND USR_RANGE ='back-user' )");
 $ad = mysqli_query($datos, "SELECT A.STSK_ID, B.USR_ID, B.USR_RANGE FROM SUBTASKS A INNER JOIN USERS B ON(A.STSK_CHARGE_USR = B.USR_ID ) WHERE (STSK_TYPE = 1 AND USR_RANGE = 'admin' AND STSK_TICKET= '" . $ticket . "' AND STSK_FAC_CODE = " . $fac . ")");
 
+
+
 while($fila = mysqli_fetch_row($ad)){
 
     $handle = opendir("/var/www/html/" . $fac . "/" . $fila[1] . "_alt/");
@@ -24,6 +26,7 @@ while($fila = mysqli_fetch_row($ad)){
              closedir($handle);
 }
 
+echo "|";
 
  mysqli_data_seek($it, 0);
  mysqli_data_seek($ad, 0);
