e4621ca5d57b44a3dae87cfc2185a02170662575
pionnertech <elaion256@outlook.com>
2015-08-18 10:14:56 -0300

    non multipart fileupload

--- backend/rewind.php
+++ backend/rewind.php
@@ -8,7 +8,7 @@ $stsk   = $_REQUEST['stsk'];
 
 // who is the main User???
 $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
-$query = mysql_fetch_assoc(mysqli_query($datos, "SELECT A.USR_ID  FROM USERS A CROSS JOIN(SELECT A.USR_DEPT as DEPT FROM USERS A WHERE A.USR_ID = " . $user . "  ) B ON(A.USR_RANGE = 'admin' AND A.USR_DEPT = B.DEPT)"));
+$query = mysqli_fetch_assoc(mysqli_query($datos, "SELECT A.USR_ID  FROM USERS A CROSS JOIN(SELECT A.USR_DEPT as DEPT FROM USERS A WHERE A.USR_ID = " . $user . "  ) B ON(A.USR_RANGE = 'admin' AND A.USR_DEPT = B.DEPT)"));
 
 $admin = $query['USR_ID'];
 
