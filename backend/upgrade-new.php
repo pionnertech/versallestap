27828e68ce57659a88bb8b882189b374ddd22194
pionnertech <elaion256@outlook.com>
2015-09-05 11:43:34 -0300

    upgrade-new

--- backend/upgrade-new.php
+++ backend/upgrade-new.php
@@ -28,7 +28,7 @@ if($argument == 0){ // si es externo
 //2.) get average
 $avg = mysqli_fetch_assoc(mysqli_query($datos, "SELECT ROUND(AVG(IFNULL(STSK_PROGRESS, 0))) AS VX FROM SUBTASKS WHERE (STSK_ISS_ID = " . $iss_id . " AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_TYPE = 0);"));
 //3.) set the avg of the total progress in admin and issue
-       mysqli_query($datos, "UPDATE SUBTASKS SET STSK_PROGRESS =  " . (int)$avg['VX'] . " WHERE (STSK_ISS_ID = " . $iss_id . " AND STSK_CHARGE_USR = STSK_MAIN_USR  AND AND STSK_TYPE = 0);");
+       mysqli_query($datos, "UPDATE SUBTASKS SET STSK_PROGRESS =  " . (int)$avg['VX'] . " WHERE (STSK_ISS_ID = " . $iss_id . " AND STSK_CHARGE_USR = STSK_MAIN_USR AND STSK_FAC_CODE = " . $fac . " AND STSK_TYPE = 0);");
      
        mysqli_query($datos, "UPDATE ISSUES SET ISS_PROGRESS = " . (int)$avg['VX'] . " WHERE (ISS_ID = " . $iss_id . " AND ISS_FAC_CODE = " . $fac . ")");
 
