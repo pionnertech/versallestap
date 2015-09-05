d0fa3b66dd69186c92151c1d2605bd7bd0a61394
pionnertech <elaion256@outlook.com>
2015-09-05 12:17:06 -0300

    value data bac-own to 0 after submit

--- Admin/other-user-profile-new.php
+++ Admin/other-user-profile-new.php
@@ -2478,10 +2478,15 @@ $("#upgrade").on('click', function (){
  
 var da  = new Date();
 var fp = da.getFullYear() + "-" + ('0' + (da.getMonth()+1)).slice(-2) + "-" + ('0' + da.getDate()).slice(-2) + " " + ('0' + da.getHours()).slice(-2) + ":" + ('0' + da.getMinutes()).slice(-2)  + ":" + ('0' + da.getSeconds()).slice(-2) ;
-if(parseInt($(".span2").val()) == $(".span2").data("Cval")) {
+
+console.info($(".span2").val()) === $(".span2").data("Cval") + "esta es la respuesta");
+
+if(parseInt($(".span2").val()) === $(".span2").data("Cval")) {
+
 bootbox.alert("Progreso debe ser mayor");
 
 } else {
+
 
  console.info("../backend/progress-ii.php?val=" + $("#value-progress").val() + 
           "&stsk_id=" + st_ii+ 
