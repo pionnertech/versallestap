100830050dd3dfa8bb959f4ef2e1508df44d8ca6
pionnertech <elaion256@outlook.com>
2015-09-05 11:42:42 -0300

    updater to sadmin

--- Admin/other-user-profile-new.php
+++ Admin/other-user-profile-new.php
@@ -3484,7 +3484,7 @@ function changeListener(){
                           
                           } else {
 
-                             $("#int-table .coll-int").eq(indice).find('input[value=u' + packets[1] + ']').next().css({ opacity : "1"});
+                             $("#int-table .coll-int").eq(indice).find('input[value=u' + packets[1] + ']').siblings("i").css({ opacity : "1"});
                           }
                             
                            
