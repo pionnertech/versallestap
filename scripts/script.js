efe35664f75976e2b7b2a21b01f034683dacfe07
pionnertech <elaion256@outlook.com>
2015-09-10 17:33:17 -0300

    greatest hits

--- scripts/script.js
+++ scripts/script.js
@@ -21,8 +21,8 @@ $(function(){
 if(data.files[0].size > 2097152){
   console.log("es mayor por que es de :" + data.files[0].size);
    
-   bootbox.alert({ buttons: "btn-danger", message: "Archivo debe ser menor a 2MB"});
-   
+   bootbox.alert({ buttons: { main:{ className : "btn-danger", label : "Aceptar" }}, message: "Archivo debe ser menor a 2MB"});
+
 } else {
             var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
                 ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span class="fa fa-times af" onclick="rewind(this)"></span></li>');
