f1f195dc6e237215040eaa3fc319e30c15376454
pionnertech <elaion256@outlook.com>
2015-09-10 16:57:52 -0300

    greatest hits

--- scripts/script-int.js
+++ scripts/script-int.js
@@ -20,8 +20,8 @@ $(function(){
         add: function (e, data) {
 
 if(data.files[0].size > 2097152){
-    
-   console.log("es mayor por que es de :" data.files[0].size);
+
+   console.log("es mayor por que es de :" + data.files[0].size);
 
    bootbox.alert("Archivo debe ser menor a 2MB!");
 
