a89b9f2d6dba0b6e4041dbde4be0297013bf5fa2
pionnertech <elaion256@outlook.com>
2015-09-10 17:24:48 -0300

    updater to sadmin

--- Front/activity.php
+++ Front/activity.php
@@ -430,6 +430,25 @@ $('.selectpicker').selectpicker({ dropupAuto : false });
 
 $(document).on('ready', function(){
 
+    $("#tel").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+             // Allow: Ctrl+C
+            (e.keyCode == 67 && e.ctrlKey === true) ||
+             // Allow: Ctrl+X
+            (e.keyCode == 88 && e.ctrlKey === true) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
 
 $("#del-wrap  div , #del-wrap input, #del-wrap h3 ").addClass('hidden');
 
@@ -438,10 +457,8 @@ $(".ctz_def").attr("disabled", true);
 $("#html5_uploader_browse").addClass('plupload_disabled');
 $("#clip").addClass('disabled');
 
-
-  // date time picker.
-
-     //google maps...
+ //google maps...
+ 
  IntializeGMaps();
  uploaderInt();
 
