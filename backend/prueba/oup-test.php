56c9df6aefcc78a60d444d0a9d1196712b13b687
pionnertech <elaion256@outlook.com>
2015-09-29 16:19:18 -0300

    rapid share

--- backend/prueba/oup-test.php
+++ backend/prueba/oup-test.php
@@ -830,7 +830,7 @@ if($handler = opendir("../" . $_SESSION['TxtFacility'] . "/" . $boss['BOSS'] . "
 
     echo "<script>console.info('" . $archivos . "    / preg_match_all(/_" . $stsk[1] . "_/, " . $archivos . ")')</script>";
 
-        if(preg_match_all("/_" . $stsk[1] . "_/", $archivos) == 1){
+        if(preg_match_all("/_\[" . $stsk[1] . "\]_/", $archivos) == 1){
 
              $extension = substr($archivos, -3);
              $cor = "";
