77ecc990f0685e92279b29442094c54dc635961d
pionnertech <elaion256@outlook.com>
2015-10-04 17:18:00 -0300

    road of the road

--- backend/prueba/oup-test.php
+++ backend/prueba/oup-test.php
@@ -870,9 +870,11 @@ if($handler = opendir("../" . $_SESSION['TxtFacility'] . "/" . $boss['BOSS'] . "
                                                       $cor = "#FF9900";
                                                       break;
                                                  }
-  echo "BREAKDOWN";
-   echo  '<a data-ttr="urre" href="../' . $_SESSION['TxtFacility'] . '/'.  $boss['BOSS'].'/' .  $archivos . '" class="file-opac" download><p class="ifile" title="' . $archivos . '"><i class="fa fa-file-' . $file_extension . 'o fa-2x" style="color:' . $cor .'"></i><span class="iname"></span></p></a>';
 
+
+
+  // echo  '<a data-ttr="urre" href="../' . $_SESSION['TxtFacility'] . '/'.  $boss['BOSS'].'/' .  $archivos . '" class="file-opac" download><p class="ifile" title="' . $archivos . '"><i class="fa fa-file-' . $file_extension . 'o fa-2x" style="color:' . $cor .'"></i><span class="iname"></span></p></a>';
+echo "<script>console.info('las vueltas de  : " . $archivos . "')</script>";
 ?>
 
                          <a href="../<? printf($_SESSION['TxtFacility']) ?>/<? printf($boss['BOSS']) ?>/<? printf($archivos) ?>" class="file-opac" download><p class="ifile" title="<? printf($archivos) ?>"><i class="fa fa-file-<? printf($file_extension) ?>o fa-2x" style="color: <? printf($cor) ?> "></i>
