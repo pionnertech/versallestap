1e4394b3f246ed964972ad90fd84ce474942da74
pionnertech <elaion256@outlook.com>
2015-10-10 23:26:29 -0300

    hitman

--- backend/prueba/file_listener.php
+++ backend/prueba/file_listener.php
@@ -1,15 +1,15 @@
 <?php
 
-while(true){
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
+
+while(true){
 
 // File Listener 
 $user = $_GET['muser'];
 $fac  = $_GET['fac'];
 
 $rdir = "/var/www/html/" . $fac . "/" . $user ;
-
 
 $fi = new FilesystemIterator($rdir, FilesystemIterator::SKIP_DOTS);
 
