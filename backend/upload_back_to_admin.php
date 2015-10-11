afef02484631f190e9441da41295e02feccf50a2
pionnertech <elaion256@outlook.com>
2015-10-10 23:29:02 -0300

    realpalm

--- backend/upload_back_to_admin.php
+++ backend/upload_back_to_admin.php
@@ -8,8 +8,12 @@ $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 
 
 //averiguar el numero de subtask matriz
+
+//get the boss ...
+$boss = mysqli_fetch_assoc(mysqli_query($datos, "SELECT USR_ID AS BOSS FROM USERS WHERE(USR_DEPT = (SELECT USR_DEPT FROm USERS WHERE USR_ID = " . $usr . ") AND USR_FACILITY = " . $fac . " AND USR_RANGE = 'admin')"));
 $hn = mysqli_fetch_assoc(mysqli_query($datos, "SELECT STSK_ISS_ID FROM SUBTASKS WHERE STSK_ID = " . $code));
-$real_code = mysqli_fetch_assoc(mysqli_query($datos, "SELECT  MIN(STSK_ID) FROM SUBTASKS WHERE ( STSK_ISS_ID = " . $hn['STSK_ISS_ID'] . " AND STSK_TYPE = 0)"));
+$real_code = mysqli_fetch_assoc(mysqli_query($datos, "SELECT  MIN(STSK_ID) FROM SUBTASKS WHERE (  STSK_ISS_ID = " . $hn['STSK_ISS_ID'] . " AND STSK_MAIN_USR = " . $boss['BOSS'] . " AND STSK_TYPE = 0)"));
+
 
 $target_dir = "/var/www/html/" . $fac . "/";
 $target_file = $target_dir . basename($_FILES["upl"]["name"]);
