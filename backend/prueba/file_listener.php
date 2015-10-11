cbd1fd55d340df4b4005ca3fbbc47494f76735e8
pionnertech <elaion256@outlook.com>
2015-10-10 23:22:46 -0300

    hitman

--- backend/prueba/file_listener.php
+++ backend/prueba/file_listener.php
@@ -9,9 +9,14 @@ $fac  = $_GET['fac'];
 
 $rdir = "/var/www/html/" . $fac . "/" . $user ;
 
+while(true){
+
 $fi = new FilesystemIterator($rdir, FilesystemIterator::SKIP_DOTS);
 
 echo iterator_count($fi) . "\n\n";
+	
+}
+
 
 flush();
 
