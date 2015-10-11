9faeb6fb37c1610efa5c7a38a6cfa32d2975d357
pionnertech <elaion256@outlook.com>
2015-10-10 23:01:16 -0300

    hitman

---
+++ backend/prueba/file_listener.php
@@ -0,0 +1,16 @@
+<?php
+
+//header('Content-Type: text/event-stream');
+//header('Cache-Control: no-cache');
+
+// File Listener 
+$user = $_GET['muser'];
+$fac  = $_GET['fac'];
+
+$rdir = "/var/www/html/" . $fac . "/" . $user . "/";
+
+
+echo shell_exec($rdir . " find . -type f | wc -l");
+
+
+?>
