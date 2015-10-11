53f4a8ce2a8b4c3982a8a1200e883351aa3829cd
pionnertech <elaion256@outlook.com>
2015-10-10 23:15:23 -0300

    console.info

--- backend/time-beta.php
+++ backend/time-beta.php
@@ -9,7 +9,7 @@ $datos = mysqli_connect('localhost', "root", "MoNoCeRoS", "K_usr10000");
 //who is listening?
 $ident = mysqli_fetch_assoc(mysqli_query($datos, "SELECT USR_RANGE AS RAN FROM USERS WHERE USR_ID = " . $usr));
 //aqui se detectan los diferenciales de progreso independiente de que origan sean 
-$first = mysqli_query($datos, "SELECT A.STSK_PROGRESS, A.STSK_ANCIENT_PRO, A.STSK_ID, A.STSK_ISS_ID, A.STSK_TYPE, B.USR_RANGE, A.STSK_TICKET, CONCAT(B.USR_NAME, ' ', B.USR_SURNAME) AS NAME, A.STSK_CHARGE_USR FROM SUBTASKS  A INNER JOIN USERS B ON (B.USR_ID = A.STSK_CHARGE_USR) WHERE (STSK_CHARGE_USR <> " . $usr . " AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR = " . $usr . " AND STSK_PROGRESS <> STSK_ANCIENT_PRO ) ORDER BY STSK_ID DESC LIMIT 1; ");
+$first = mysqli_query($datos, "SELECT A.STSK_PROGRESS, A.STSK_ANCIENT_PRO, A.STSK_ID, A.STSK_ISS_ID, A.STSK_TYPE, B.USR_RANGE, A.STSK_TICKET, CONCAT(B.USR_NAME, ' ', B.USR_SURNAME) AS NAME, A.STSK_CHARGE_USR, A.STSK_OVER FROM SUBTASKS  A INNER JOIN USERS B ON (B.USR_ID = A.STSK_CHARGE_USR) WHERE (STSK_CHARGE_USR <> " . $usr . " AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR = " . $usr . " AND STSK_PROGRESS <> STSK_ANCIENT_PRO ) ORDER BY STSK_ID DESC LIMIT 1; ");
 //si no encuentra, salga
 if(mysqli_num_rows($first) < 1){
 
@@ -37,7 +37,7 @@ switch ($sel['STSK_TYPE']) {
 	  // determine if is Finished
 	   if($sel['STSK_PROGRESS'] > 99.9){  
 	      	$classText = "FINALIZADO"; } else {  
-	     	$classText = "" ;
+	      	$classText = "" ;
 	     }
 	   
 
@@ -47,13 +47,21 @@ switch ($sel['STSK_TYPE']) {
 	   $total = mysqli_fetch_assoc(mysqli_query($datos, "SELECT ROUND(AVG(CASE WHEN STSK_PROGRESS IS NULL THEN 0 ELSE STSK_PROGRESS END)) AS AVX FROM SUBTASKS WHERE (STSK_TICKET = '" . $sel['STSK_TICKET'] . "' AND STSK_CHARGE_USR <> STSK_MAIN_USR AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR =  " . $usr . ")" ));
        //get the INFO about the partial progress
        $trf_str = mysqli_fetch_assoc(mysqli_query($datos, "SELECT * FROM TRAFFIC WHERE TRF_STSK_ID = " . $sel['STSK_ID'] . " ORDER BY TRF_ID DESC"));
+       //get the REAL TRF_ISS_SRC_ID depending of STSK_OVER
+  
+          if($sel['STSK_OVER'] == 1){
+
+             $real = $sel['STSK_ID'];
+          } else {
+             $real = $trf_str['TRF_STSK_SRC_ID'];
+          }
        
                 echo str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($sel['NAME']))));
                 echo "|" . $trf_str['TRF_USER'];
                 echo "|" . $trf_str['TRF_SUBJECT'];
                 echo "|" . $trf_str['TRF_DESCRIPT'];
                 echo "|" . date('d/m/Y', strtotime( $trf_str['TRF_ING_DATE']));
-                echo "|" . $trf_str['TRF_STSK_SRC_ID'];
+                echo "|" . $real;
                 echo "|" . $total['AVX'];
                 echo "|" . $classText;
                 echo "|" . $trf_str['TRF_STSK_ID']; 
