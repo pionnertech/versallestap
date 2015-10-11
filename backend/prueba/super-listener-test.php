1934ef48abfc3b4b2da577888d198f1c087b3347
pionnertech <elaion256@outlook.com>
2015-10-10 23:11:02 -0300

    gear near

--- backend/prueba/super-listener-test.php
+++ backend/prueba/super-listener-test.php
@@ -23,26 +23,28 @@ echo "|" . $user_out5;
 exit;
 
 }
+
+
+
 $post_trigger = mysqli_fetch_assoc($trigger);
 //output the general and individual progress
-$query = mysqli_query($datos, "SELECT A.STSK_PROGRESS, A.STSK_ANCIENT_PRO, A.STSK_ID, A.STSK_ISS_ID, A.STSK_TYPE, B.USR_RANGE, A.STSK_TICKET  FROM SUBTASKS  A INNER JOIN USERS B ON (B.USR_ID = A.STSK_CHARGE_USR) WHERE (USR_RANGE = 'sadmin' AND STSK_TYPE = 0 AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR = "  . $usr . " AND STSK_TICKET = '" . $post_trigger['PSD_TICKET'] ."')");
+
+if(substr($trigger['PSD_TICKET'], 0, 2) == 'EX'){
+
+
+$query = mysqli_query($datos, "SELECT A.ISS_PROGRESS,  A.ISS_ID, A.STSK_TYPE, B.USR_RANGE, A.ISS_TICKET  FROM SUBTASKS  A INNER JOIN USERS B ON (B.USR_ID = A.STSK_CHARGE_USR) WHERE (USR_RANGE = 'sadmin' AND STSK_TYPE = 0 AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR = "  . $usr . " AND STSK_TICKET = '" . $post_trigger['PSD_TICKET'] ."')");
 $query_assoc = mysqli_fetch_assoc($query);
 
-//what is the progress?
-
-  $add = mysqli_fetch_assoc(mysqli_query($datos, "SELECT  SUM(A.STSK_PROGRESS), COUNT(A.STSK_ID), ROUND(AVG(IFNULL(A.STSK_PROGRESS, 0))) AS PROGRESS FROm SUBTASKS A INNER JOIN USERS B ON(B.USR_RANGE ='admin' AND A.STSK_CHARGE_USR = B.USR_ID) WHERE (STSK_FAC_CODE = " . $fac . " AND STSK_TICKET = '" . $query_assoc['STSK_TICKET'] . "')")); 
-
-//who is?, which percentage?,
- 
-$handler = mysqli_fetch_assoc(mysqli_query($datos, "SELECT PSD_ID, PSD_USR, PSD_PERCENT FROM PSEUDO WHERE (PSD_FAC_CODE = " . $fac . " AND PSD_TICKET = '" . $query_assoc['STSK_TICKET'] . "') ORDER BY PSD_TIMESTAMP DESC" ));
-
-//transform all into readable info
-$trfm = mysqli_fetch_assoc(mysqli_query($datos, "SELECT CONCAT(USR_NAME, ' ' , USR_SURNAME) AS NAME FROM USERS WHERE USR_ID = " . $handler['PSD_USR'] . ";"));
+//what is the progress
+ $add = mysqli_fetch_assoc(mysqli_query($datos, "SELECT  SUM(A.STSK_PROGRESS), COUNT(A.STSK_ID), ROUND(AVG(IFNULL(A.STSK_PROGRESS, 0))) AS PROGRESS FROm SUBTASKS A INNER JOIN USERS B ON(B.USR_RANGE ='admin' AND A.STSK_CHARGE_USR = B.USR_ID) WHERE (STSK_FAC_CODE = " . $fac . " AND STSK_TICKET = '" . $query_assoc['ISS_TICKET'] . "')")); 
+ $handler = mysqli_fetch_assoc(mysqli_query($datos, "SELECT PSD_ID, PSD_USR, PSD_PERCENT FROM PSEUDO WHERE (PSD_FAC_CODE = " . $fac . " AND PSD_TICKET = '" . $query_assoc['ISS_TICKET'] . "') ORDER BY PSD_TIMESTAMP DESC" ));
+ $trfm = mysqli_fetch_assoc(mysqli_query($datos, "SELECT CONCAT(USR_NAME, ' ' , USR_SURNAME) AS NAME FROM USERS WHERE USR_ID = " . $handler['PSD_USR'] . ";"));
 
 
-$user_out1 = str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($trfm['NAME']))));
-$user_out2 = $handler['PSD_USR'];
-$user_out3 = $handler['PSD_ID'];
+
+$user_out1 = str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($trfm['NAME'])))); //Nombre del usuario admin
+$user_out2 = $handler['PSD_USR']; // id del user
+$user_out3 = $handler['PSD_ID']; //
 $user_out4 = $handler['PSD_PERCENT'];
 $user_out5 = $add['PROGRESS'];
 $user_out6 = $post_trigger['PSD_TICKET'];
@@ -60,6 +62,55 @@ echo "|" . $user_out5;
 echo "|" . $user_out6 . "|";
 
 }
+
+
+
+} else {
+
+$query = mysqli_query($datos, "SELECT A.STSK_PROGRESS, A.STSK_ANCIENT_PRO, A.STSK_ID, A.STSK_ISS_ID, A.STSK_TYPE, B.USR_RANGE, A.STSK_TICKET  FROM SUBTASKS  A INNER JOIN USERS B ON (B.USR_ID = A.STSK_CHARGE_USR) WHERE (USR_RANGE = 'sadmin' AND STSK_TYPE = 0 AND STSK_FAC_CODE = " . $fac . " AND STSK_MAIN_USR = "  . $usr . " AND STSK_TICKET = '" . $post_trigger['PSD_TICKET'] ."')");
+$query_assoc = mysqli_fetch_assoc($query);
+
+//what is the progress?
+
+  $add = mysqli_fetch_assoc(mysqli_query($datos, "SELECT  SUM(A.STSK_PROGRESS), COUNT(A.STSK_ID), ROUND(AVG(IFNULL(A.STSK_PROGRESS, 0))) AS PROGRESS FROm SUBTASKS A INNER JOIN USERS B ON(B.USR_RANGE ='admin' AND A.STSK_CHARGE_USR = B.USR_ID) WHERE (STSK_FAC_CODE = " . $fac . " AND STSK_TICKET = '" . $query_assoc['STSK_TICKET'] . "')")); 
+
+//who is?, which percentage?,
+ 
+$handler = mysqli_fetch_assoc(mysqli_query($datos, "SELECT PSD_ID, PSD_USR, PSD_PERCENT FROM PSEUDO WHERE (PSD_FAC_CODE = " . $fac . " AND PSD_TICKET = '" . $query_assoc['STSK_TICKET'] . "') ORDER BY PSD_TIMESTAMP DESC" ));
+
+//transform all into readable info
+$trfm = mysqli_fetch_assoc(mysqli_query($datos, "SELECT CONCAT(USR_NAME, ' ' , USR_SURNAME) AS NAME FROM USERS WHERE USR_ID = " . $handler['PSD_USR'] . ";"));
+
+
+$user_out1 = str_replace('\' ', '\'', ucwords(str_replace('\'', '\' ', strtolower($trfm['NAME'])))); //Nombre del usuario admin
+$user_out2 = $handler['PSD_USR']; // id del user
+$user_out3 = $handler['PSD_ID']; //
+$user_out4 = $handler['PSD_PERCENT'];
+$user_out5 = $add['PROGRESS'];
+$user_out6 = $post_trigger['PSD_TICKET'];
+
+if(!mysqli_query($datos, "DELETE FROM PSEUDO WHERE PSD_ID = " . $handler['PSD_ID']  )){
+	  mysqli_error($datos);
+
+} else {
+
+echo "|" . $user_out1;
+echo "|" . $user_out2;
+echo "|" . $user_out3;
+echo "|" . $user_out4;
+echo "|" . $user_out5;
+echo "|" . $user_out6 . "|";
+
+}
+
+}
+
+
+
+
+
+
+
 
 
 
