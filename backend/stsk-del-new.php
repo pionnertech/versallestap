d7a9d152e958a44a2cee02269cdf91d3cccd1568
pionnertech <elaion256@outlook.com>
2015-10-10 23:09:57 -0300

    realpalm

--- backend/stsk-del-new.php
+++ backend/stsk-del-new.php
@@ -18,7 +18,18 @@ if(!mysqli_query($datos, "UPDATE SUBTASKS SET STSK_RESP = 0 WHERE (STSK_TYPE= 0 
   exit ;
 }
 
+
+
+
+
+$over = mysqli_fetch_assoc(mysqli_query($datos, "SELECT ISS_CHARGE_USR AS SW FROM ISSUES WHERE ISS_ID = " . $iss_id));
 // ticket  plus
+
+if($over['SW'] == $muser){
+ $sr = 1;
+} else {
+ $sr = 0;
+}
 
 $ticket = mysqli_fetch_assoc(mysqli_query($datos, "SELECT STSK_TICKET FROm SUBTASKS  WHERE STSK_ID =" . $stsk_id));
 
@@ -30,8 +41,8 @@ if($user == "Mi Departamento"){
     $uteam = mysqli_query($datos, "SELECT USR_ID FROM USERS WHERE (USR_DEPT = '" . $dept['USR_DEPT'] . "' AND USR_FACILITY =  "  . $fac . " AND USR_RANGE = 'back-user' )" );
 
     while($fila = mysqli_fetch_row($uteam)){
-        $query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_STATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE, STSK_PROGRESS, STSK_TYPE, STSK_LOCK, STSK_TICKET) ";
-        $query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', " . $fila[0] . ", '" . $fechaF . "', 1 ,  '" . $startD . "' , " . $muser . ", " . $fac . ", 0, 0, 1, '" . $ticket['STSK_TICKET']. "')";
+        $query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_STATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE, STSK_PROGRESS, STSK_TYPE, STSK_LOCK, STSK_TICKET, STSK_OVER) ";
+        $query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', " . $fila[0] . ", '" . $fechaF . "', 1 ,  '" . $startD . "' , " . $muser . ", " . $fac . ", 0, 0, 1, '" . $ticket['STSK_TICKET']. "', " . $sr .")";
          mysqli_query($datos, $query);
          echo $fila[0] . "|";
          }
@@ -42,8 +53,8 @@ if($user == "Mi Departamento"){
   for($i= 0; $i < count($uteam_array); $i++ ){
   	  $usr_id_q = mysqli_fetch_assoc(mysqli_query($datos, "SELECT USR_ID FROM USERS WHERE (CONCAT(USR_NAME, ' ' , USR_SURNAME) = '" . strtoupper($uteam_array[$i]) . "' AND USR_FACILITY = " . $fac .")"));
       
-       $query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_STATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE, STSK_PROGRESS, STSK_TYPE, STSK_LOCK, STSK_TICKET) ";
-       $query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', '" . $usr_id_q['USR_ID'] . "', '" . $fechaF . "', 1 ,  '" . $startD . "' , '" . $muser . "', " . $fac . ", 0, 0, 1, '" . $ticket['STSK_TICKET'] . "')";
+       $query = "INSERT INTO SUBTASKS (STSK_ISS_ID, STSK_SUBJECT, STSK_DESCRIP ,STSK_CHARGE_USR, STSK_FINISH_DATE, STSK_STATE, STSK_START_DATE, STSK_MAIN_USR, STSK_FAC_CODE, STSK_PROGRESS, STSK_TYPE, STSK_LOCK, STSK_TICKET, STSK_OVER) ";
+       $query .= "VALUES (" . $iss_id . ", '" . $subject . "', '" . $descript . "', '" . $usr_id_q['USR_ID'] . "', '" . $fechaF . "', 1 ,  '" . $startD . "' , '" . $muser . "', " . $fac . ", 0, 0, 1, '" . $ticket['STSK_TICKET'] . "', " . $sr .")";
   	   mysqli_query($datos, $query);
   	   echo $usr_id_q['USR_ID'] . "|";
   }
