a030ec04aed7c267ee3554fac534d471b9dbf8d0
pionnertech <elaion256@outlook.com>
2015-10-15 18:09:29 -0300

    paradoja

--- Front/task-new.php
+++ Front/task-new.php
@@ -27,8 +27,7 @@ $query_str_issues = "SELECT A.ISS_ID, " .
  "A.ISS_COMENTARY " .
 "FROM ISSUES A INNER JOIN EST B ON(A.ISS_STATE = B.EST_CODE) INNER JOIN USERS E ON(E.USR_ID = A.ISS_CHARGE_USR)" .
 "INNER JOIN CITIZENS C ON(C.CTZ_RUT = A.ISS_CTZ AND C.CTZ_FAC_ENTER = " . $_SESSION['TxtFacility'] . ") " .
-"INNER JOIN CAT D ON(D.CAT_ID = A.ISS_TYPE)  WHERE A.ISS_FAC_CODE = " . $_SESSION['TxtFacility'] . " ORDER BY A.ISS_ID;";
-
+"INNER JOIN CAT D ON(D.CAT_ID = A.ISS_TYPE)  WHERE (A.ISS_FAC_CODE = " . $_SESSION['TxtFacility'] . " AND ISS_OVER = 0 )ORDER BY A.ISS_ID;";
 
 $Query_task = mysqli_query($datos,$query_str_issues );
 $Query_alerts_ext = mysqli_query($datos, "SELECT COUNT(ISS_ID), ISS_STATE FROM ISSUES WHERE ISS_FAC_CODE = " . $_SESSION['TxtFacility'] . " GROUP BY ISS_STATE");
